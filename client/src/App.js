3c67543c8eb312f246c78cde2fde7ee415e2d454
Arjun Sahjpaul <a.sahjpaul@gmail.com>
2023-12-11 20:45:03 -0800

    added basic folder and code structure, logo svg

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import Header from "./components/Header/Header";
+import Hero from './components/Hero/Hero';
+import SheltersCard from "./components/SheltersCard/SheltersCard";
+import SheltersMap from "./components/SheltersMap/SheltersMap";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (

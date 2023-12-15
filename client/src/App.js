3e3f4350d4edba137f5085fdb0cbea0e5c17b973
Arjun Sahjpaul <a.sahjpaul@gmail.com>
2023-12-14 20:20:00 -0800

    updated App.js to include updated component structure

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from '../src/components/Header/Header';
 import Hero from '../src/components/Hero/Hero';
 import OurMission from './components/OurMission/OurMission';
+import Parallax from './components/Parallax/Parallax';
 import SheltersCard from './components/SheltersCard/SheltersCard';
 import SheltersMap from './components/SheltersMap/SheltersMap';
 import Footer from './components/Footer/Footer';
@@ -12,6 +13,7 @@ export default function App() {
       <Header />
       <Hero />
       <OurMission />
+      <Parallax />
       <SheltersCard />
       {/* <SheltersMap />
       <Footer /> */}

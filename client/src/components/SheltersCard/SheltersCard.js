a8da0120c10e17856df6dde8c4e4bff01748ffd6
Arjun Sahjpaul <a.sahjpaul@gmail.com>
2023-12-15 01:41:17 -0800

    added total of 6 records to be displayed on web app, confirmed working 🚀🚀🚀

--- client/src/components/SheltersCard/SheltersCard.js
+++ client/src/components/SheltersCard/SheltersCard.js
@@ -21,7 +21,16 @@ export default function FetchData() {
           {records.map(record => ( // Mapping over records
             //Using _id as a key
             <li key={record._id}>
-              {record.LOCATION_NAME} | {record.LOCATION_ADDRESS}
+              <br />
+              <ul>Organization Name: {record.ORGANIZATION_NAME}</ul>
+              <ul>Shelter Group: {record.SHELTER_GROUP}</ul>
+              <ul>Location Name: {record.LOCATION_NAME}</ul>
+              <ul>Location Address: {record.LOCATION_ADDRESS}</ul>
+              <ul>Unoccupied Beds: {record.UNOCCUPIED_BEDS}</ul>
+              <ul>Occupancy Date: {record.OCCUPANCY_DATE}</ul>
+              <br />
+
+              {/* {record.LOCATION_NAME} | {record.LOCATION_ADDRESS} | {record.UNOCCUPIED_BEDS} */}
             </li>
           ))}
         </ul>

cc2e1c209f59829e11fae55b11dc1283d8b89080
Arjun Sahjpaul <a.sahjpaul@gmail.com>
2023-12-14 18:41:26 -0800

    updated OurMission Styling

--- client/src/components/OurMission/OurMission.js
+++ client/src/components/OurMission/OurMission.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './OurMission.scss';
+import Logo from '../../assets/logo/SafeHavenTO.svg';
 
 export default function HeroPurpose() {
   const [scrollPosition, setScrollPosition] = useState(0);
@@ -20,21 +21,26 @@ export default function HeroPurpose() {
     <>
       <section class="mission__section">
         <div class="mission__div">
-          <h2 class="mission__div-header">Our Mission</h2>
-          <br />
-
-            <p class="mission__div-text">
-            SafeHavenTO is a web app that aims to help individuals experiencing homelessness by finding nearby shelters based on their needs and the latest data from nearby shelters.  This app leverages the City of Toronto's Daily Shelter Occupancy Data API.
-            </p>
+          <div class="mission__div-inner">
+            <h2 class="mission__div-header">Our Mission</h2>
             <br />
-            <p class="mission__div-text">
-            Our mission is to help bridge the gap between those in need and the resources available to them; to serve as a beacon of hope in addressing this pressing societal issue.  {' '}
-            </p>
 
+            <p class="mission__div-text">
+              SafeHavenTO is a web app that aims to help individuals
+              experiencing homelessness by finding nearby shelters based on
+              their needs and the latest data from nearby shelters. This app
+              leverages the City of Toronto's Daily Shelter Occupancy Data API.
+            </p>
+            <p class="mission__div-text">
+              Our mission is to help bridge the gap between those in need and
+              the resources available to them; to serve as a beacon of hope in
+              addressing this pressing societal issue.{' '}
+            </p>
+            {/* <img className="mission__div-logo" src={Logo}></img> */}
+          </div>
         </div>
 
-
-      <div class="mission__parallax"></div>
+        <div class="mission__parallax"></div>
       </section>
     </>
   );

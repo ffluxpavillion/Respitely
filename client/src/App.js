7b9209090b1a64a2e33701b7d77ab04867c68fb2
Arjun Sahjpaul <a.sahjpaul@gmail.com>
2023-12-14 17:34:26 -0800

    updated app components

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,22 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from '../src/components/Header/Header';
 import Hero from '../src/components/Hero/Hero';
-import SheltersCard from "./components/SheltersCard/SheltersCard";
-import SheltersMap from "./components/SheltersMap/SheltersMap";
-import Footer from "./components/Footer/Footer";
-
+import OurMission from './components/OurMission/OurMission';
+import SheltersCard from './components/SheltersCard/SheltersCard';
+import SheltersMap from './components/SheltersMap/SheltersMap';
+import Footer from './components/Footer/Footer';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Header />
       <Hero />
-      <Routes>
+      <OurMission />
+      <SheltersCard />
+      {/* <SheltersMap />
+      <Footer /> */}
 
-      </Routes>
-
+      <Routes></Routes>
     </BrowserRouter>
   );
 }
-

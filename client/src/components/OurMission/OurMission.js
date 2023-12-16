a683501afa0c5bbf08cc89e0e05a690188956042
Arjun Sahjpaul <a.sahjpaul@gmail.com>
2023-12-15 19:00:12 -0800

    fixed className syntax typo

--- client/src/components/OurMission/OurMission.js
+++ client/src/components/OurMission/OurMission.js
@@ -6,19 +6,19 @@ export default function HeroPurpose() {
 
   return (
     <>
-      <section class="mission__section">
-        <div class="mission__div">
-          <div class="mission__div-inner">
-            <h2 class="mission__div-header">Our Mission</h2>
+      <section className="mission__section">
+        <div className="mission__div">
+          <div className="mission__div-inner">
+            <h2 className="mission__div-header">Our Mission</h2>
             <br />
 
-            <p class="mission__div-text">
+            <p className="mission__div-text">
               SafeHavenTO is a web app that aims to help individuals
               experiencing homelessness by finding nearby shelters based on
               their needs and the latest data from nearby shelters. This app
               leverages the City of Toronto's Daily Shelter Occupancy Data API.
             </p>
-            <p class="mission__div-text">
+            <p className="mission__div-text">
               Our mission is to help bridge the gap between those in need and
               the resources available to them; to serve as a beacon of hope in
               addressing this pressing societal issue.{' '}

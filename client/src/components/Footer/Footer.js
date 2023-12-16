e7139cf00e6bc04d0636ab20b751063789e65446
Arjun Sahjpaul <a.sahjpaul@gmail.com>
2023-12-16 14:58:05 -0800

    added footer image, set styling for image an footer text

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,10 +1,15 @@
 import './Footer.scss'
+import FooterImage from '../../assets/images/SafeHavenTO_graphic-art-community-1.png';
+
 export default function Footer () {
 
   return (
-    <footer>
-        <p>&copy; 2023 SafeHavenTO. All rights reserved.</p>
-    </footer>
+    <>
+      <footer classname="footer">
+            <div className="hero" style={{ backgroundImage: `url(${FooterImage})` }} />
+          <p className="copyright">&copy; 2023 SafeHavenTO. All rights reserved.</p>
+      </footer>
+    </>
   )
 
 

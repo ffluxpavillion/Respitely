efe8e5e344b0056ae3834afecbd25de5aa5fe95b
Arjun Sahjpaul <a.sahjpaul@gmail.com>
2023-12-16 15:03:00 -0800

    updated image filepath

--- client/src/components/Hero/Hero.js
+++ client/src/components/Hero/Hero.js
@@ -1,18 +1,12 @@
 import './Hero.scss';
-import Banner from '../../assets/images/SafeHavenTO-banner.png';
-import Banner2 from '../../assets/images/SafeHavenTO.png';
-
-
+import HeroImage from '../../assets/images/SafeHavenTO_graphic-art-skyline-1.png';
 
 export default function Hero() {
-
-
+  
   return (
     <>
     <div>
-      {/* <div className="hero" style={{ backgroundImage: `url(${Banner})` }}>
-       */}
-       <div className="hero" style={{ backgroundImage: `url(${Banner2})` }} />
+       <div className="hero" style={{ backgroundImage: `url(${HeroImage})` }} />
 
           <div className="hero__div">
             <h1 className="hero__div-h1">SafeHavenTO</h1>

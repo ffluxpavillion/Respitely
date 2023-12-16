bcfbc25304346370dfd22e84cdf2472366a4f8c1
Arjun Sahjpaul <a.sahjpaul@gmail.com>
2023-12-16 15:21:07 -0800

    updated component render

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Header from '../src/components/Header/Header';
 import Hero from '../src/components/Hero/Hero';
 import OurMission from './components/OurMission/OurMission';
 import Parallax from './components/Parallax/Parallax';
+import Parallax2 from './components/Parallax2/Parallax2';
 import SheltersCard from './components/SheltersCard/SheltersCard';
 import SheltersMap from './components/SheltersMap/SheltersMap';
 import Footer from './components/Footer/Footer';
@@ -10,15 +11,15 @@ import Footer from './components/Footer/Footer';
 export default function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <Hero />
-      <OurMission />
-      <Parallax />
-      <SheltersCard />
-      {/* <SheltersMap />
-      <Footer /> */}
-
-      <Routes></Routes>
-    </BrowserRouter>
+        <Header />
+        <Hero />
+        <OurMission />
+        <Parallax />
+        <SheltersCard />
+        <SheltersMap />
+        <Parallax2 />
+        <Footer />
+        <Routes></Routes>
+      </BrowserRouter>
   );
 }

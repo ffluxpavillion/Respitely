7cc4e5f7b87489f368c2a50e6779ef012e25bac1
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-01-03 20:35:45 -0800

    Added MapBox functionality to map over locations and display marker on map 📍

--- client/src/components/SheltersCard/SheltersCard.js
+++ client/src/components/SheltersCard/SheltersCard.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import SheltersMap from '../SheltersMap/SheltersMap';
 
-export default function SheltersCard(record) {
+export default function SheltersCard() {
   const [loading, setLoading] = useState(true); // state to show/hide Loading Shelter Data message
   const [records, setRecords] = useState([]);
   const [displayedRecords, setDisplayedRecords] = useState([]);
@@ -11,24 +11,63 @@ export default function SheltersCard(record) {
   const [filterType, setFilterType] = useState('All'); // new state for filter type
   const [selectedButton, setSelectedButton] = useState(null);
   const [showLoadMore, setShowLoadMore] = useState(false); // state to show/hide Load More button
+  // const [uniqueLocations, setUniqueLocations] = useState({}); // piece of state to store unique locations
 
-  // fetch data from server
   useEffect(() => {
     const fetchData = async () => {
-    await axios
-      .get('http://localhost:8080/shelters/')
-      .then((response) => {
-        // console.log(response.data[0].CAPACITY_ACTUAL_BED);
-        filterAndSortData(response.data, 'All');
-        setTimeout(() => { // display Loading Shelter Data message for 1 second
-          setLoading(false);
-        }, 1000);
-      })
-      .catch((err) => console.error(err));
+      axios
+        .get('http://localhost:8080/shelters/')
+        .then((response) => {
+          // // Process the data to create unique locations
+          // const processedData = processData(response.data);
+          // // console.log('processedData is', processedData);
+          // setUniqueLocations(processedData); // Update the state
+
+          filterAndSortData(response.data, 'All');
+          // display Loading Shelter Data message for 1 second
+          setTimeout(() => {
+            setLoading(false);
+          }, 1000);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
     };
 
     fetchData();
   }, []);
+
+  // Alternative method to process data to create unique locations
+  //   const processData = (data) => {
+  //     const uniqueLocations = {};
+
+  //     data.forEach((item, index) => {
+  //       const address = item.LOCATION_ADDRESS;
+  //       // const shelterGroup = item.SHELTER_GROUP;
+  //       if (!uniqueLocations[address]) {
+  //         uniqueLocations[index] = {
+  //           id: index,
+  //           shelterGroup: item.SHELTER_GROUP,
+  //           address: item.LOCATION_ADDRESS,
+  //           city: item.LOCATION_CITY,
+  //           province: item.LOCATION_PROVINCE,
+  //           postalCode: item.LOCATION_POSTAL_CODE,
+  //           occupancyDate: item.OCCUPANCY_DATE,
+  //           sector: item.SECTOR,
+  //           capacityType: item.CAPACITY_TYPE,
+  //           programModel: item.PROGRAM_MODEL,
+  //           overnightServiceType: item.OVERNIGHT_SERVICE_TYPE,
+  //           unoccupiedBeds: item.UNOCCUPIED_BEDS,
+  //           unoccupiedRooms: item.UNOCCUPIED_ROOMS,
+  //         };
+  //       }
+  //       // console.log('uniqueLocations is: ', uniqueLocations[address]);
+
+  //     });
+
+  //     return uniqueLocations;
+  //   };
+  // console.log('uniqueLocations =', uniqueLocations);
 
   useEffect(() => {
     // Set a timeout to show the Load More button after 2 seconds
@@ -36,7 +75,7 @@ export default function SheltersCard(record) {
       setShowLoadMore(true);
     }, 2000);
 
-    return () => clearTimeout(timer);    // Clean up function to clear the timeout
+    return () => clearTimeout(timer); // Clean up function to clear the timeout
   }, []);
 
   // function to filter data based on CAPACITY_TYPE, and then sort based on OCCUPANCY_DATE
@@ -44,25 +83,31 @@ export default function SheltersCard(record) {
     const fetchedData = JSON.parse(JSON.stringify(data)); // deep copy data to avoid mutating state
     let filteredData;
     if (type === 'Beds') {
-      // console.log(`Before filtering for ${type}:`, data); // troubleshooting data mutation
       filteredData = fetchedData
         .filter((record) => record.CAPACITY_TYPE === 'Bed Based Capacity')
-        .sort((a, b) => parseInt(b.UNOCCUPIED_BEDS, 10) - parseInt(a.UNOCCUPIED_BEDS, 10));
-      // console.log(`After filtering for ${type}:`, filteredData); // troubleshooting data mutation
+        .sort(
+          (a, b) =>
+            parseInt(b.UNOCCUPIED_BEDS, 10) - parseInt(a.UNOCCUPIED_BEDS, 10)
+        );
     } else if (type === 'Rooms') {
-      // console.log(`Before filtering for ${type}:`, data); // troubleshooting data mutation
       filteredData = fetchedData
         .filter((record) => record.CAPACITY_TYPE === 'Room Based Capacity')
-        .sort((a, b) => parseInt(b.UNOCCUPIED_ROOMS, 10) - parseInt(a.UNOCCUPIED_ROOMS, 10));
-      // console.log(`After filtering for ${type}:`, filteredData); // troubleshooting data mutation
+        .sort(
+          (a, b) =>
+            parseInt(b.UNOCCUPIED_ROOMS, 10) - parseInt(a.UNOCCUPIED_ROOMS, 10)
+        );
     } else {
-      // console.log(`Before filtering for ${type}:`, data); // troubleshooting data mutation
       filteredData = fetchedData.sort((a, b) => {
-        const maxA = Math.max(parseInt(a.UNOCCUPIED_BEDS || '0', 10), parseInt(a.UNOCCUPIED_ROOMS || '0', 10));
-        const maxB = Math.max(parseInt(b.UNOCCUPIED_BEDS || '0', 10), parseInt(b.UNOCCUPIED_ROOMS || '0', 10));
+        const maxA = Math.max(
+          parseInt(a.UNOCCUPIED_BEDS || '0', 10),
+          parseInt(a.UNOCCUPIED_ROOMS || '0', 10)
+        );
+        const maxB = Math.max(
+          parseInt(b.UNOCCUPIED_BEDS || '0', 10),
+          parseInt(b.UNOCCUPIED_ROOMS || '0', 10)
+        );
         return maxB - maxA;
       });
-      // console.log(`After filtering for ${type}:`, filteredData); // troubleshooting data mutation
     }
 
     setRecords(fetchedData); // set newly sorted records
@@ -81,31 +126,50 @@ export default function SheltersCard(record) {
     setSelectedButton(button);
   };
 
+  // Console logs for debugging purposes
+  // console.log('displayed records= ', displayedRecords.map(record => record.LOCATION_ADDRESS));
+  // console.log('displayedRecords:', displayedRecords);
+  // console.log('filtered data:', filteredData);
+  // console.log('records = :', records);
 
   return (
     <>
       <section className="sheltersCard__section" id="shelters">
         <div className="sheltersCard__div">
-          <h1 className="sheltersCard__div-h2">Latest Shelter Occupancy in Toronto</h1>
+          <h1 className="sheltersCard__div-h2">
+            Latest Shelter Occupancy in Toronto
+          </h1>
           <h2>Filter By:</h2>
-          <button className={`sheltersCard__Btn btn--All ${selectedButton === 'All' ? 'selected' : ''}`}
-            onClick={() => { // onClick, filter and sort data, and set selected button
+          <button
+            className={`sheltersCard__Btn btn--All ${
+              selectedButton === 'All' ? 'selected' : ''
+            }`}
+            onClick={() => {
+              // onClick, filter and sort data, and set selected button
               filterAndSortData(records, 'All');
               handleClick('All');
             }}
           >
             <h3>All</h3>
           </button>
-          <button className={`sheltersCard__Btn btn--Beds ${selectedButton === 'Beds' ? 'selected' : ''}`}
-            onClick={() => { // onClick, filter and sort data, and set selected button
+          <button
+            className={`sheltersCard__Btn btn--Beds ${
+              selectedButton === 'Beds' ? 'selected' : ''
+            }`}
+            onClick={() => {
+              // onClick, filter and sort data, and set selected button
               filterAndSortData(records, 'Beds');
               handleClick('Beds');
             }}
           >
             <h3>Beds</h3>
           </button>
-          <button className={`sheltersCard__Btn btn--Rooms ${selectedButton === 'Rooms' ? 'selected' : ''}`}
-            onClick={() => { // onClick, filter and sort data, and set selected button
+          <button
+            className={`sheltersCard__Btn btn--Rooms ${
+              selectedButton === 'Rooms' ? 'selected' : ''
+            }`}
+            onClick={() => {
+              // onClick, filter and sort data, and set selected button
               filterAndSortData(records, 'Rooms');
               handleClick('Rooms');
             }}
@@ -117,94 +181,125 @@ export default function SheltersCard(record) {
           <div className="scrollable-container">
             <div className="shelterInfo__div">
               <ul className="shelterInfo__div-ul">
-                {loading ? <h1 className="loading-message">Loading Shelter Data...</h1> :
-                  displayedRecords && displayedRecords.map((record ) => ( // first, checks if displayedRecords data exists, then maps through the data
-                    //Using _id as key
-                    <li className="shelterInfo__div-li" key={record._id}>
+                {loading ? (
+                  <h1 className="loading-message">Loading Shelter Data...</h1>
+                ) : (
+                  displayedRecords &&
+                  displayedRecords.map(
+                    (
+                      record // first, checks if displayedRecords data exists, then maps through the data
+                    ) => (
+                      //Using _id as key
+                      <li className="shelterInfo__div-li" key={record._id}>
                         <div className="shelterInfo__div-left">
                           <ul className="shelterInfo__div-left-inner">
-                            <h3 className="shelterInfo__div-h3">Shelter Group: </h3>
-                              <p>{record.SHELTER_GROUP}</p>
-                                </ul>
+                            <h3 className="shelterInfo__div-h3">
+                              Shelter Group:{' '}
+                            </h3>
+                            <p>{record.SHELTER_GROUP}</p>
+                          </ul>
                           <ul className="shelterInfo__div-left-inner">
                             <h3 className="shelterInfo__div-h3">Address: </h3>
-                              <p id='locationAddress'>{record.LOCATION_ADDRESS}</p>
-                                </ul>
+                            <p id="locationAddress">
+                              {record.LOCATION_ADDRESS}
+                            </p>
+                          </ul>
                           <ul className="shelterInfo__div-left-inner">
                             <h3 className="shelterInfo__div-h3">City: </h3>
-                              <p>{record.LOCATION_CITY}</p>
-                                </ul>
+                            <p>{record.LOCATION_CITY}</p>
+                          </ul>
                           <ul className="shelterInfo__div-left-inner">
-                            <h3 className="shelterInfo__div-h3">Postal Code: </h3>
+                            <h3 className="shelterInfo__div-h3">
+                              Postal Code:{' '}
+                            </h3>
                             <br />
-                              <p>{record.LOCATION_POSTAL_CODE}</p>
-                                </ul>
+                            <p>{record.LOCATION_POSTAL_CODE}</p>
+                          </ul>
                           <ul className="shelterInfo__div-right-inner">
                             <br />
-                              <h3 className="shelterInfo__div-h3">Last Updated: </h3>
-                              <br />
-                              <h3>{record.OCCUPANCY_DATE}</h3>
-                                </ul>
+                            <h3 className="shelterInfo__div-h3">
+                              Last Updated:{' '}
+                            </h3>
+                            <br />
+                            <h3>{record.OCCUPANCY_DATE}</h3>
+                          </ul>
                         </div>
                         <div className="shelterInfo__div-right">
                           <ul className="shelterInfo__div-left-inner">
-                              <h3 className="shelterInfo__div-h3">Sector: </h3>
-                                <p>{record.SECTOR}</p>
-                                  </ul>
+                            <h3 className="shelterInfo__div-h3">Sector: </h3>
+                            <p>{record.SECTOR}</p>
+                          </ul>
                           <ul className="shelterInfo__div-right-inner">
                             <h3 className="shelterInfo__div-h3">Type: </h3>
-                              <p>{record.CAPACITY_TYPE}</p>
-                                </ul>
+                            <p>{record.CAPACITY_TYPE}</p>
+                          </ul>
                           <ul className="shelterInfo__div-right-inner">
-                            <h3 className="shelterInfo__div-h3">Program Model: </h3>
-                              <p>{record.PROGRAM_MODEL}</p>
-                              <p>{record.OVERNIGHT_SERVICE_TYPE}</p>
-                                </ul>
+                            <h3 className="shelterInfo__div-h3">
+                              Program Model:{' '}
+                            </h3>
+                            <p>{record.PROGRAM_MODEL}</p>
+                            <p>{record.OVERNIGHT_SERVICE_TYPE}</p>
+                          </ul>
 
-                          { // if CAPACITY_TYPE is Bed Based Capacity, display UNOCCUPIED_BEDS
-                            record.CAPACITY_TYPE === 'Bed Based Capacity'
-                              ?
-                                (<ul className="shelterInfo__div-right-inner">
-                                  <h3 className="shelterInfo__div-h3">Available Beds: </h3>
-                                    <p>{record.UNOCCUPIED_BEDS}</p></ul>)
-                              : ('')
+                          {
+                            // if CAPACITY_TYPE is Bed Based Capacity, display UNOCCUPIED_BEDS
+                            record.CAPACITY_TYPE === 'Bed Based Capacity' ? (
+                              <ul className="shelterInfo__div-right-inner">
+                                <h3 className="shelterInfo__div-h3">
+                                  Available Beds:{' '}
+                                </h3>
+                                <p>{record.UNOCCUPIED_BEDS}</p>
+                              </ul>
+                            ) : (
+                              ''
+                            )
                           }
-                          { // if CAPACITY_TYPE is Room Based Capacity, display UNOCCUPIED_ROOMS
-                            record.CAPACITY_TYPE === 'Room Based Capacity'
-                            ?
-                            (<ul className="shelterInfo__div-right-inner">
-                              <h3 className="shelterInfo__div-h3">Available Rooms: </h3>
-                                <p>{record.UNOCCUPIED_ROOMS}</p></ul>)
-                            : ('')
+                          {
+                            // if CAPACITY_TYPE is Room Based Capacity, display UNOCCUPIED_ROOMS
+                            record.CAPACITY_TYPE === 'Room Based Capacity' ? (
+                              <ul className="shelterInfo__div-right-inner">
+                                <h3 className="shelterInfo__div-h3">
+                                  Available Rooms:{' '}
+                                </h3>
+                                <p>{record.UNOCCUPIED_ROOMS}</p>
+                              </ul>
+                            ) : (
+                              ''
+                            )
                           }
-
                         </div>
 
-                      <div className="actions__div">
-                        <button className="btn--Directions">
-                          <h3>Directions</h3>
-                        </button>
-                        <button className="btn--Share">
-                          <h3>Share</h3>
-                        </button>
-                      </div>
-                    </li>
+                        <div className="actions__div">
+                          <button className="btn--Directions">
+                            <h3>Directions</h3>
+                          </button>
+                          <button className="btn--Share">
+                            <h3>Share</h3>
+                          </button>
+                        </div>
+                      </li>
+                    )
                   )
                 )}
               </ul>
             </div>
-            { // if loadCount is less than records.length, display load more button
-              loadCount < records.length && showLoadMore &&
-              (
+            {
+              // if loadCount is less than records.length, display load more button
+              loadCount < records.length && showLoadMore && (
                 <button
                   onClick={loadMore}
-                  className="loadMoreBtn btn--LoadMore">
-                    Load More
+                  className="loadMoreBtn btn--LoadMore"
+                >
+                  Load More
                 </button>
               )
             }
           </div>
-          <SheltersMap  />
+          <SheltersMap
+            locations={displayedRecords}
+            records={records}
+            filterType={filterType}
+          />
         </div>
       </section>
     </>
--- client/src/components/SheltersMap/SheltersMap.js
+++ client/src/components/SheltersMap/SheltersMap.js
@@ -1,47 +1,86 @@
 import './SheltersMap.scss';
-import GoogleMapReact from 'google-map-react';
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
+import Map, {
+  NavigationControl,
+  ScaleControl,
+  GeolocateControl,
+  Marker,
+  Source,
+  Layer,
+} from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import axios from 'axios';
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
-
-export default function SheltersMap () {
+export default function SheltersMap(props) {
   // piece of state to store maps API key
-  const [apiKey, setApiKey] = useState("");
+  const [apiKey, setApiKey] = useState('');
+  const [locations, setLocations] = useState([]);
 
   useEffect(() => {
     // fetch and set maps key from server
-    fetch('http://localhost:8080/api/maps-key')
-  .then(response => response.json())
-  .then(data => {
-    setApiKey(data.key);
-  });
+    axios.get('http://localhost:8080/api/maps-key').then((response) => {
+      setApiKey(response.data.key);
+    });
   }, []);
 
-  const defaultProps = {
-    // set default center of map to Toronto
-    center: {
-      lat: 43.653908,
-      lng: -79.384293
-    },
-    zoom: 13
-  };
+  useEffect(() => {
+    const fetchGeocode = async () => {
+      if (!apiKey) return;
+      // fetch geocode for each location
+      const newLocations = await Promise.all(
+        props.records.map(async (location) => {
+          const fullAddress = `${location.LOCATION_ADDRESS} ${location.LOCATION_CITY} ${location.LOCATION_PROVINCE} ${location.LOCATION_POSTAL_CODE}`;
+          const encodedAddress = encodeURIComponent(fullAddress); // encode address for URL
+          try {
+            const response = await axios.get(
+              `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodedAddress}.json?access_token=${apiKey}`
+            );
+            const coordinates = response.data.features[0].geometry.coordinates;
+            return { ...location, coordinates };
+          } catch (error) {
+            console.error('Error fetching geocode:', error);
+            return null;
+          }
+        })
+      );
+
+      // updating the locations state with the new array, and only include locations with valid geocoded data --this removes any locations where geocoding failed
+      setLocations(newLocations.filter((loc) => loc != null));
+    };
+
+    fetchGeocode();
+  }, [apiKey, props.records]);
 
   return (
     <>
-        <div className="sheltersMap__Container">
-        {apiKey && <GoogleMapReact
-          bootstrapURLKeys={{ key: apiKey }}
-          defaultCenter={defaultProps.center}
-          defaultZoom={defaultProps.zoom}
+      {apiKey && (
+        <Map
+          mapboxAccessToken={apiKey}
+          initialViewState={{
+            longitude: -79.384293,
+            latitude: 43.653908,
+            zoom: 12.1,
+          }}
+          style={{ width: '45vw', height: '40vw' }}
+          mapStyle="mapbox://styles/mapbox/streets-v9"
         >
-          <AnyReactComponent
-            lat={43.653908}
-            lng={-79.384293}
-            text="My Marker"
-          />
-        </GoogleMapReact>}
-    </div>
-    </>
+          <NavigationControl />
+          <ScaleControl />
+          <GeolocateControl />
 
+          {locations.map(
+            (location, index) =>
+              location.coordinates && (
+                <Marker
+                  className="marker"
+                  key={index}
+                  latitude={location.coordinates[1]}
+                  longitude={location.coordinates[0]}
+                />
+              )
+          )}
+        </Map>
+      )}
+    </>
   );
 }

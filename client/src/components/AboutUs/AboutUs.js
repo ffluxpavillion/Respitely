6f2a46ee270940f1f2e7b1feab4e20effffec0bd
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-01-14 22:05:39 -0800

    fixed typos / formatting

--- client/src/components/AboutUs/AboutUs.js
+++ client/src/components/AboutUs/AboutUs.js
@@ -20,7 +20,7 @@ export default function AboutUs() {
             <p className="mission__div-text">
               Our mission is to help bridge the gap between those in need and
               the resources available to them; to serve as a beacon of hope in
-              addressing this pressing societal issue.{' '}
+              addressing this pressing societal issue.
             </p>
             <p className="mission__div-text">
              The homelessness crisis is riddled with multifaceted challenges, one such obstacle is the access to timely and accurate shelter information. There's a lack of centralized, up-to-date resources for individuals in need, causing difficulties in finding suitable shelters, especially during urgent situations.
@@ -29,7 +29,7 @@ export default function AboutUs() {
               By seamlessly integrating modern design and functionality with an intuitive and reliable user experience (UX), such an application holds the potential to revolutionize the way individuals experiencing homelessness access vital information.  This platform aims to provide more than just basic information, but also a sense of security during times of intense vulnerability.
             </p>
             <div className="mission__div-logo-container">
-            <img className="mission__div-logo" src={Logo}></img>
+              <img className="mission__div-logo" src={Logo}></img>
             </div>
           </div>
         </div>

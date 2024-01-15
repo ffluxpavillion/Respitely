afa620757be1a9a400d54e70c5b433f8775e4116
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-01-14 22:24:08 -0800

    Feature -- completely redesigned nav, and styles updated/refactored to be fully responsive for mobile, tablet, and desktop.

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,33 +1,49 @@
-import { useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import './Header.scss';
 import Logo from '../../assets/logo/SafeHavenTO.svg';
-import {HashLink as Link} from 'react-router-hash-link';
+import { HashLink as Link } from 'react-router-hash-link';
 
 export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => { // toggles menu open and closed
+    setMenuOpen(!menuOpen);
+  };
+
+  const handleClick = () => { // closes menu when link is clicked
+    setMenuOpen(false);
+  };
+
+  useEffect(() => { // enable user to close nav via ESC key
+    const closeOnEscape = (e) => {
+      if (e.key === 'Escape') {
+        setMenuOpen(false);
+      }
+    };
+
+    document.addEventListener('keydown', closeOnEscape);
+
+    return () => document.removeEventListener('keydown', closeOnEscape); // cleanup eventListener
+  }, []);
 
   return (
-    <>
-      <header className="header" id="home">
-        <nav
-        // ref={navbarRef} // Attach the ref to the navbar
-        className="navbar">
-          <div className="navbar__div">
-            <img className="navbar__div-logo" src={Logo}></img>
-            <li>
-              <Link to='#home'><h1 className="navbar__div-item">Home</h1></Link>
-            </li>
-            <li>
-              <Link to='#aboutUs'><h1 className="navbar__div-item">About Us</h1></Link>
-            </li>
-            <li>
-              <Link to='#shelters'><h1 className="navbar__div-item">Shelters</h1></Link>
-            </li>
-            <li>
-              <Link to='#resources'><h1 className="navbar__div-item">Resources</h1></Link>
-            </li>
+    <header className="header" id="home">
+      <nav className="navbar">
+        <div className="navbar__div">
+          <img className="navbar__div-logo" src={Logo} alt="Logo"></img>
+          <ul className={`navbar__div-ul navMenu ${menuOpen ? 'active' : ''}`}>
+            <li onClick={handleClick}><Link to='#home'><h1 className="navbar__div-li">Home</h1></Link></li>
+            <li onClick={handleClick}><Link to='#aboutUs'><h1 className="navbar__div-li">About Us</h1></Link></li>
+            <li onClick={handleClick}><Link to='#shelters'><h1 className="navbar__div-li">Shelters</h1></Link></li>
+            <li onClick={handleClick}><Link to='#resources'><h1 className="navbar__div-li">Resources</h1></Link></li>
+          </ul>
+          <div className={`navbar__burger-menu menu ${menuOpen ? 'active' : ''}`} onClick={toggleMenu}>
+            <span className='navbar__burger-bar bar'></span>
+            <span className='navbar__burger-bar bar'></span>
+            <span className='navbar__burger-bar bar'></span>
           </div>
-        </nav>
-      </header>
-    </>
+        </div>
+      </nav>
+    </header>
   );
 }

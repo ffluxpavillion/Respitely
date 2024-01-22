bbaf349f4a0a46e8b5d3f7267c120fe601818d7d
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-01-21 18:24:01 -0800

    disabled img logo, fixed typo

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import './Header.scss';
-import Logo from '../../assets/logo/SafeHavenTO.svg';
+// import Logo from '../../assets/logo/SafeHavenTO.svg';
 import { HashLink as Link } from 'react-router-hash-link';
 
 export default function Header() {
@@ -30,14 +30,14 @@ export default function Header() {
     <header className="header" id="home">
       <nav className="navbar">
         <div className="navbar__div">
-          <img className="navbar__div-logo" src={Logo} alt="Logo"></img>
+          {/* <img className="navbar__div-logo" src={Logo} alt="Logo"></img> */}
           <ul className={`navbar__div-ul navMenu ${menuOpen ? 'active' : ''}`}>
             <li onClick={handleClick}><Link to='#home'><h1 className="navbar__div-li">Home</h1></Link></li>
             <li onClick={handleClick}><Link to='#aboutUs'><h1 className="navbar__div-li">About Us</h1></Link></li>
             <li onClick={handleClick}><Link to='#shelters'><h1 className="navbar__div-li">Shelters</h1></Link></li>
             <li onClick={handleClick}><Link to='#resources'><h1 className="navbar__div-li">Resources</h1></Link></li>
           </ul>
-          <div className={`navbar__burger-menu menu ${menuOpen ? 'active' : ''}`} onClick={toggleMenu}>
+          <div className={`navbar__burger menu ${menuOpen ? 'active' : ''}`} onClick={toggleMenu}>
             <span className='navbar__burger-bar bar'></span>
             <span className='navbar__burger-bar bar'></span>
             <span className='navbar__burger-bar bar'></span>

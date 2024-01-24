2285cb02b756f77495cef71e9c1e278cb8391de4
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-01-23 20:46:05 -0800

    Added Resources section to be fully responsive for Mobile, Tablet, and Desktop devices, added link animations

--- client/src/components/Resources/Resources.js
+++ client/src/components/Resources/Resources.js
@@ -9,66 +9,72 @@ export default function Resources() {
       <section className="resources__section">
         <h2 className="resources__div-header" id="resources">Resources</h2>
         <div className="resources__div">
-            <br />
+          <br />
             <div className="grid-container">
-              <div className="grid-item">
-                <h1 className="link">
-                  <a className="link" href="https://www.toronto.ca/community-people/housing-shelter/homeless-help/shelters/#shelters">
+              <a className="link" href="https://www.toronto.ca/community-people/housing-shelter/homeless-help/shelters/#shelters">
+                <div className="grid__item">
+                  <h1 className="grid__item-header">
                     Shelters
-                  </a>
-                </h1>
-                <p>Emergency accommodation and related support services that assist people to move into housing.</p>
-              </div>
-              <div className="grid-item">
-                <h1 className="link">
-                  <a className="link" href="https://www.toronto.ca/community-people/housing-shelter/homeless-help/24-hour-respite-sites/#respitesites">
-                    24-Hour Respite Sites
-                  </a>
-                </h1>
-                <p>Low-barrier services, providing resting spaces, meals and service referrals. Open 24/7.</p>
-              </div><div className="grid-item">
-                <h1 className="link">
-                  <a className="link" href="https://www.toronto.ca/community-people/housing-shelter/homeless-help/drop-ins/#dropin">
-                  Drop-In Programs
-                  </a>
-                </h1>
-                <p>Basic necessity services to people who are homeless or at risk of homelessness.</p>
-              </div><div className="grid-item">
-                <h1 className="link">
-                  <a className="link" href="https://www.toronto.ca/community-people/housing-shelter/homeless-help/streets-to-homes-street-outreach-support-program/">
-                  Street Outreach
-                  </a>
-                </h1>
-                <p>Services for people experiencing homelessness and living outdoors.</p>
-              </div><div className="grid-item">
-                <h1 className="link">
-                  <a className="link" href="https://www.toronto.ca/community-people/housing-shelter/homeless-help/toronto-warming-centres/">
-                  Warming Centres
-                  </a>
-                </h1>
-                <p>Warm spaces for vulnerable residents to come inside during extreme cold weather.</p>
-              </div><div className="grid-item">
-                <h1 className="link">
-                  <a className="link" href="https://www.toronto.ca/community-people/housing-shelter/homeless-help/housing-help/">
-                  Housing Help
-                  </a>
-                </h1>
-                <p>Non-profit agencies that help people find and keep housing, and avoid eviction</p>
-              </div><div className="grid-item">
-                <h1 className="link">
-                  <a className="link" href="https://www.toronto.ca/community-people/housing-shelter/homeless-help/pet-services-in-homeless-shelters/">
-                  Pet Services for Clients of Homeless Services
-                  </a>
-                </h1>
-                <p>No-charge services for pets of people staying at shelter sites.</p>
-              </div><div className="grid-item">
-                <h1 className="link">
-                  <a className="link" href="https://www.toronto.ca/explore-enjoy/recreation/washrooms-drinking-water-in-parks-recreational-facilities/#location=&lat=&lng=">
-                  Public Washrooms & Showers
-                  </a>
-                </h1>
-                <p>Locations of public washrooms in parks and recreational facilities.</p>
-              </div>
+                  </h1>
+                  <p className="grid__item-text">Emergency accommodation and related support services that assist people to move into housing.</p>
+                </div>
+              </a>
+              <a className="link" href="https://www.toronto.ca/community-people/housing-shelter/homeless-help/24-hour-respite-sites/#respitesites">
+                <div className="grid__item">
+                  <h1 className="grid__item-header">
+                    24-Hour <br />Respite Sites
+                  </h1>
+                  <p className="grid__item-text">Low-barrier services, providing resting spaces, meals and service referrals. Open 24/7.</p>
+                </div>
+              </a>
+              <a className="link" href="https://www.toronto.ca/community-people/housing-shelter/homeless-help/drop-ins/#dropin">
+                <div className="grid__item">
+                  <h1 className="grid__item-header">
+                    Drop-In Programs
+                  </h1>
+                  <p className="grid__item-text">Basic necessity services to people who are homeless or at risk of homelessness.</p>
+                </div>
+              </a>
+              <a className="link" href="https://www.toronto.ca/community-people/housing-shelter/homeless-help/streets-to-homes-street-outreach-support-program/">
+                <div className="grid__item">
+                  <h1 className="grid__item-header">
+                    Street Outreach
+                  </h1>
+                  <p className="grid__item-text">Services for people experiencing homelessness and living outdoors.</p>
+                </div>
+              </a>
+              <a className="link" href="https://www.toronto.ca/community-people/housing-shelter/homeless-help/toronto-warming-centres/">
+                <div className="grid__item">
+                  <h1 className="grid__item-header">
+                    Warming Centres
+                  </h1>
+                  <p className="grid__item-text">Warm spaces for vulnerable residents to come inside during extreme cold weather.</p>
+                </div>
+              </a>
+              <a className="link" href="https://www.toronto.ca/community-people/housing-shelter/homeless-help/housing-help/">
+                <div className="grid__item">
+                  <h1 className="grid__item-header">
+                    Housing Help
+                  </h1>
+                  <p className="grid__item-text">Non-profit agencies that help people find and keep housing, and avoid eviction</p>
+                </div>
+              </a>
+              <a className="link" href="https://www.toronto.ca/community-people/housing-shelter/homeless-help/pet-services-in-homeless-shelters/">
+                <div className="grid__item">
+                  <h1 className="grid__item-header">
+                    Pet Services <br />for clients of <br />Homeless Services
+                  </h1>
+                  <p className="grid__item-text">No-charge services for pets of people staying at shelter sites.</p>
+                </div>
+              </a>
+              <a className="link" href="https://www.toronto.ca/explore-enjoy/recreation/washrooms-drinking-water-in-parks-recreational-facilities/#location=&lat=&lng=">
+                <div className="grid__item">
+                  <h1 className="grid__item-header">
+                    Public Washrooms & Showers
+                  </h1>
+                  <p className="grid__item-text">Locations of public washrooms in parks and recreational facilities.</p>
+                </div>
+              </a>
             </div>
           </div>
       </section>

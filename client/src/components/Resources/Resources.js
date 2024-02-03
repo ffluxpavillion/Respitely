c2d6f10fc8a919a5ddbb6b395c4484836263c897
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-02-02 20:29:05 -0800

    Updated Resources component styling --added gradient background and updated some animations

--- client/src/components/Resources/Resources.js
+++ client/src/components/Resources/Resources.js
@@ -1,77 +1,77 @@
 import React, { useState, useEffect } from 'react';
 import './Resources.scss';
-import Logo from '../../assets/logo/SafeHavenTO.svg';
+
 
 export default function Resources() {
 
   return (
     <>
       <section className="resources__section">
-        <h2 className="resources__div-header" id="resources">Resources</h2>
+        <h1 className="resources__div-header" id="resources">Resources</h1>
         <div className="resources__div">
           <br />
             <div className="grid-container">
               <a className="link" target='_blank' href="https://www.toronto.ca/community-people/housing-shelter/homeless-help/shelters/#shelters">
                 <div className="grid__item">
-                  <h1 className="grid__item-header">
+                  <h2 className="grid__item-header">
                     Shelters
-                  </h1>
+                  </h2>
                   <p className="grid__item-text">Emergency accommodation and related support services that assist people to move into housing.</p>
                 </div>
               </a>
               <a className="link" target='_blank' href="https://www.toronto.ca/community-people/housing-shelter/homeless-help/24-hour-respite-sites/#respitesites">
                 <div className="grid__item">
-                  <h1 className="grid__item-header">
+                  <h2 className="grid__item-header">
                     24-Hour <br />Respite Sites
-                  </h1>
+                  </h2>
                   <p className="grid__item-text">Low-barrier services, providing resting spaces, meals and service referrals. Open 24/7.</p>
                 </div>
               </a>
               <a className="link" target='_blank' href="https://www.toronto.ca/community-people/housing-shelter/homeless-help/drop-ins/#dropin">
                 <div className="grid__item">
-                  <h1 className="grid__item-header">
+                  <h2 className="grid__item-header">
                     Drop-In Programs
-                  </h1>
+                  </h2>
                   <p className="grid__item-text">Basic necessity services to people who are homeless or at risk of homelessness.</p>
                 </div>
               </a>
               <a className="link" target='_blank' href="https://www.toronto.ca/community-people/housing-shelter/homeless-help/streets-to-homes-street-outreach-support-program/">
                 <div className="grid__item">
-                  <h1 className="grid__item-header">
+                  <h2 className="grid__item-header">
                     Street Outreach
-                  </h1>
+                  </h2>
                   <p className="grid__item-text">Services for people experiencing homelessness and living outdoors.</p>
                 </div>
               </a>
               <a className="link" target='_blank' href="https://www.toronto.ca/community-people/housing-shelter/homeless-help/toronto-warming-centres/">
                 <div className="grid__item">
-                  <h1 className="grid__item-header">
+                  <h2 className="grid__item-header">
                     Warming Centres
-                  </h1>
+                  </h2>
                   <p className="grid__item-text">Warm spaces for vulnerable residents to come inside during extreme cold weather.</p>
                 </div>
               </a>
               <a className="link" target='_blank' href="https://www.toronto.ca/community-people/housing-shelter/homeless-help/housing-help/">
                 <div className="grid__item">
-                  <h1 className="grid__item-header">
+                  <h2 className="grid__item-header">
                     Housing Help
-                  </h1>
+                  </h2>
                   <p className="grid__item-text">Non-profit agencies that help people find and keep housing, and avoid eviction</p>
                 </div>
               </a>
               <a className="link" target='_blank' href="https://www.toronto.ca/community-people/housing-shelter/homeless-help/pet-services-in-homeless-shelters/">
                 <div className="grid__item">
-                  <h1 className="grid__item-header">
+                  <h2 className="grid__item-header">
                     Pet Services <br />for clients of <br />Homeless Services
-                  </h1>
+                  </h2>
                   <p className="grid__item-text">No-charge services for pets of people staying at shelter sites.</p>
                 </div>
               </a>
               <a className="link" target='_blank' href="https://www.toronto.ca/explore-enjoy/recreation/washrooms-drinking-water-in-parks-recreational-facilities/#location=&lat=&lng=">
                 <div className="grid__item">
-                  <h1 className="grid__item-header">
+                  <h2 className="grid__item-header">
                     Public Washrooms & Showers
-                  </h1>
+                  </h2>
                   <p className="grid__item-text">Locations of public washrooms in parks and recreational facilities.</p>
                 </div>
               </a>

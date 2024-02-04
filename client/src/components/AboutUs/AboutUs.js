2bf135cbd3fff837d4dbc41e77cd9e8d9331c4ee
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-02-04 00:39:42 -0800

    Updated text and refactored styling --added gradient background, font updates, and improved overall responsiveness

--- client/src/components/AboutUs/AboutUs.js
+++ client/src/components/AboutUs/AboutUs.js
@@ -8,44 +8,55 @@ export default function AboutUs() {
     <>
       <section className="mission__section">
       <br />
-
         <div className="mission__section-div">
-
           <h1 className="mission__section-div-h1" id='aboutUs' >safehaven•TO</h1>
         </div>
         <br />
         <br />
         <br />
-        
         {/* <h1 className="mission__section__div-h1" >modern problems</h1>
         <h1 className="mission__section__div-h1" >require</h1>
         <h1 className="mission__section__div-h1" >modern solutions</h1> */}
-
         <div className="mission__div">
           <div className="mission__div-inner">
-            <h2 className="mission__div-h2">Mission Statement</h2>
+            <div className="mission__div-inner-container-1">
+              <h2 className="mission__div-h2">Moderninzing Shelter Access</h2>
+              <h2 className="mission__div-h2">in the Heart of Toronto</h2>
+            </div>
             <br />
-            <p className="mission__div-text">
-              SafeHavenTO is a full-stack & responsive web application designed to help individuals
-              experiencing homelessness find nearby shelters based on
-              their individual needs, and the latest updates from locations around the city. This app
-              leverages JSON data provided by the City of Toronto's Daily Shelter Occupancy Data API,
-              via the city's Open Data Portal initiative.
-            </p>
-            <p className="mission__div-text">
-              Our mission is to help bridge the gap between those in need and
-              the resources available to them; to serve as a beacon of hope in
-              addressing this pressing societal issue.
-            </p>
-            <p className="mission__div-text">
-             The homelessness crisis is riddled with multifaceted challenges, one such obstacle is the access to timely and accurate shelter information. There is a lack of centralized, up-to-date resources for individuals in need, causing difficulties in finding suitable shelters, especially during urgent situations.
-            </p>
-            <p className="mission__div-text">
-              By seamlessly integrating modern design and functionality with an intuitive and reliable user experience (UX), such an application holds the potential to revolutionize the way individuals experiencing homelessness access vital information.
-            </p>
-            <p className="mission__div-text">
-              This platform aims to provide more than just basic information, but also a sense of security during times of intense vulnerability.
-            </p>
+            <div className="mission__div-inner-container-2">
+              <p className="mission__div-text">
+                  SafeHavenTO is a full-stack & responsive web application designed
+                  to help those experiencing homelessness in finding nearby shelters
+                  based on their individual needs, and the latest updates from locations
+                  around the city. This app leverages data provided by the City of
+                  Toronto's Daily Shelter Occupancy Data API, via the city's Open Data
+                  Portal initiative.
+                <br />
+                <br />
+                Our mission is to help bridge the gap between those in need and
+                the resources available to them; to serve as a beacon of hope in
+                addressing this pressing societal issue.
+                <br />
+                <br />
+                The homelessness crisis is riddled with multifaceted challenges,
+                one such obstacle is the access to timely and accurate shelter
+                information. There is a lack of centralized, up-to-date resources
+                for individuals in need, causing difficulties in finding suitable
+                shelters, especially during urgent situations.
+                <br />
+                <br />
+                By seamlessly integrating modern design and functionality with an
+                intuitive and reliable user experience, such an application
+                holds the potential to revolutionize the way individuals experiencing
+                homelessness access vital information.
+                <br />
+                <br />
+                This platform aims to provide critical information, but also
+                a sense of security during times of intense vulnerability. We believe that
+                everyone deserves a SafeHaven, and we are committed to making that a reality.
+              </p>
+            </div>
             <div className="mission__div-logo-container">
               <img className="mission__div-logo" src={Logo}></img>
             </div>

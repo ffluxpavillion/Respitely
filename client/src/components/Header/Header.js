fec9ba682cd38ab2ab8588434dbaa65da911f253
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-02-09 15:27:57 -0800

    Code cleanup

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import './Header.scss';
-// import Logo from '../../assets/logo/SafeHavenTO.svg';
 import { HashLink as Link } from 'react-router-hash-link';
 
 export default function Header() {
@@ -30,7 +29,6 @@ export default function Header() {
     <header className="header" id="home">
       <nav className="navbar">
         <div className="navbar__div">
-          {/* <img className="navbar__div-logo" src={Logo} alt="Logo"></img> */}
           <ul className={`navbar__div-ul navMenu ${menuOpen ? 'active' : ''}`}>
             <li onClick={handleClick}><Link to='#home'><h3 className="navbar__div-li">Home</h3></Link></li>
             <li onClick={handleClick}><Link to='#aboutUs'><h3 className="navbar__div-li">About</h3></Link></li>

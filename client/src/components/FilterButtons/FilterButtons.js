06d2803b728f754b70cbd27fa9e5273f8feca2bb
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-02-09 23:49:32 -0800

    Refactored filter buttons functionality to exist as its own component --effort to improve readability and practice separation of concerns

---
+++ client/src/components/FilterButtons/FilterButtons.js
@@ -0,0 +1,40 @@
+import './FilterButtons.scss';
+import BedIcon from '../../assets/icons/SafeHavenTO_icon-bed.svg'
+import RoomIcon from '../../assets/icons/SafeHavenTO_icon-room.svg'
+
+const FilterButtons = ({ selectedButton, filterAndSortData, handleClick, records }) => {
+  return (
+    <>
+      <button className={`sheltersCard__Btn btn--Beds ${selectedButton === 'Beds' ? 'selected' : ''}`}
+        onClick={() => {
+          filterAndSortData(records, 'Beds');
+          handleClick('Beds');
+        }}
+      >
+        <h3 className='btn--Text'> <img className='btn--Icon' src={BedIcon} alt="Bed Icon" />Beds</h3>
+      </button>
+      <button
+        className={`sheltersCard__Btn btn--Rooms ${selectedButton === 'Rooms' ? 'selected' : ''}`}
+        onClick={() => {
+          filterAndSortData(records, 'Rooms');
+          handleClick('Rooms');
+        }}
+      >
+        <h3 className='btn--Text'><img className='btn--Icon' src={RoomIcon} alt="Room Icon" />Rooms</h3>
+      </button>
+      <button className={`sheltersCard__Btn btn--All ${selectedButton === 'All' ? 'selected' : ''}`}
+        onClick={() => {
+          filterAndSortData(records, 'All');
+          handleClick('All');
+        }}
+      >
+        <h3 className='btn--Text'>
+          <img className='btn--Icon btn--Filter-All' src={BedIcon} alt="Bed Icon" />
+          <img className='btn--Icon btn--Filter-All' src={RoomIcon} alt="Room Icon" />
+          All</h3>
+      </button>
+    </>
+  );
+};
+
+export default FilterButtons;

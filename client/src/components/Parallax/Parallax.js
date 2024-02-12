105240181ff1eb097b98e2b00cee7ec4a302ed10
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-02-12 16:03:00 -0500

    Code cleanup

--- client/src/components/Parallax/Parallax.js
+++ client/src/components/Parallax/Parallax.js
@@ -1,27 +1,10 @@
-import { useState, useEffect } from 'react';
 import './Parallax.scss';
 import ParallaxImage from '../../assets/images/SafeHavenTO_ttc-queen-station.jpg';
 
-
-export default function Hero2() {
-  const [scrollPosition, setScrollPosition] = useState(0);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrollPosition(window.scrollY);
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
+export default function Parallax() {
   return (
     <>
       <div className="parallax" style={{ backgroundImage: `url(${ParallaxImage})` }}></div>
     </>
-
   );
 }
--- client/src/components/Parallax2/Parallax2.js
+++ client/src/components/Parallax2/Parallax2.js
@@ -1,27 +1,10 @@
-import { useState, useEffect } from 'react';
 import './Parallax2.scss';
 import ParallaxImage2 from '../../assets/images/SafeHavenTO_graphic-art-skyline-3.png';
 
-
 export default function Parallax2() {
-  const [scrollPosition, setScrollPosition] = useState(0);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrollPosition(window.scrollY);
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
   return (
     <>
       <div className="parallax2" style={{ backgroundImage: `url(${ParallaxImage2})` }}></div>
     </>
-
   );
 }

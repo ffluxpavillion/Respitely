0941cef419310e9db33a52fb1455e005729d0b59
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-05-06 16:07:25 -0400

    Add Home component Landing Page, with styles and animations

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,41 @@
+import './Home.scss';
+import React from 'react'
+import { HashLink as Link } from 'react-router-hash-link';
+
+
+
+export default function Home() {
+
+  return (
+    <>
+      <section className='home-section'>
+        <div className="home-header__container">
+          <div className='home-header__text'>
+            <h1 className='home-header__line1'>Find Shelter In Toronto.</h1>
+              {/* <br /> */}
+            <h1 className='home-header__line2'>Today.</h1>
+          </div>
+
+          {/* <div className='home-header__text-layer'>
+            <h1 className='home-header__line1'>Find Shelter In Toronto.</h1>
+            <h1 className='home-header__line2'>Today.</h1>
+          </div> */}
+        </div>
+
+        <div className='home-button__container'>
+          <div class="center-con">
+            <span className='header-actions__text'>Jump to Shelters Map </span>
+              <Link to='#shelters'>
+                <div class="round">
+                  <div id="cta">
+                    <span class="arrow primera next "></span>
+                    <span class="arrow segunda next "></span>
+                  </div>
+                </div>
+                </Link>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}

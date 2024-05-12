ac6165d1dd540a76524bd1c9d75babdd8d7abd79
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-05-11 20:45:29 -0400

    Update Parallax2 and add Parallax3 components and their styles

---
+++ client/src/components/Parallax3/Parallax3.js
@@ -0,0 +1,11 @@
+import './Parallax3.scss';
+import ParallaxImage from '../../assets/images/SafeHavenTO_ttc-queen-station.jpg';
+
+
+export default function Parallax3() {
+  return (
+    <>
+      <div className="parallax" style={{ backgroundImage: `url(${ParallaxImage})` }}></div>
+    </>
+  );
+}

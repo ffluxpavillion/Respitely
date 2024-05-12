12acdfcc585b99d36bde331c63390725ea203dcd
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-05-11 20:47:55 -0400

    Update App.js and App.scss with new components and styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,29 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from '../src/components/Header/Header';
-import Hero from '../src/components/Hero/Hero';
+import Home from '../src/components/Home/Home';
+import Parallax1 from './components/Parallax1/Parallax1';
 import AboutUs from './components/AboutUs/AboutUs';
-import Parallax from './components/Parallax/Parallax';
+import Parallax3 from './components/Parallax3/Parallax3';
 import Parallax2 from './components/Parallax2/Parallax2';
 import SheltersCard from './components/SheltersCard/SheltersCard';
 import Resources from './components/Resources/Resources';
 import Footer from './components/Footer/Footer';
 import './App.scss';
+import { FloatButton } from 'antd';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Hero />
-      <AboutUs />
-      <Parallax />
+      <Home />
+      <Parallax1 />
       <SheltersCard />
       <Parallax2 />
       <Resources />
+      <Parallax3 />
+      <AboutUs />
       <Footer />
+      <FloatButton.BackTop />
     </BrowserRouter>
   );
 }

dacd1977dce929b42907fda2146f9214e87c316d
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-05-16 23:37:54 -0400

    Add BuyMeACoffee component and styles

---
+++ client/src/components/BuyMeACoffee/BuyMeACoffee.js
@@ -0,0 +1,13 @@
+import './BuyMeACoffee.scss';
+
+export default function BuyMeACoffee() {
+  return (
+    <div>
+      <a href="https://www.buymeacoffee.com/safehavento" target="_blank" rel="noopener noreferrer">
+        <button className="buy-me-a-coffee-button">
+          ☕ Buy me a coffee
+        </button>
+      </a>
+    </div>
+  )
+}

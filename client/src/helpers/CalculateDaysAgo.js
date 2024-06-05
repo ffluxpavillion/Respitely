3f0c24c3d0b46f4f252d57ba842048584b6de0ec
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-06-05 16:21:51 -0400

    Refactored CalculateDaysAgo into isolated helper function at client/src/helpers/CalculateDaysAgo.js

---
+++ client/src/helpers/CalculateDaysAgo.js
@@ -0,0 +1,14 @@
+export default function calculateDaysAgo(dateString) {
+  const date = new Date(dateString);
+  const today = new Date();
+  const timeDifference = today.getTime() - date.getTime();
+  const daysDifference = Math.floor(timeDifference / (1000 * 3600 * 24));
+
+  if (daysDifference === 0) {
+    return '(Today)';
+  } else if (daysDifference === 1) {
+    return '(1 Day Ago)';
+  } else {
+    return `(${daysDifference} Days Ago)`;
+  }
+}

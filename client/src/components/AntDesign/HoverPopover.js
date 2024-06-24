120d7ea4f2f38ab9c46a9abb52bbd54a0f0577a6
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-06-24 03:23:39 -0400

    refactor: Improve HoverPopover component with additional props and class names

--- client/src/components/AntDesign/HoverPopover.js
+++ client/src/components/AntDesign/HoverPopover.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { Button, Popover } from 'antd';
 
-export default function HoverPopover({ content, title, buttonText }) {
+export default function HoverPopover({
+  content,
+  title,
+  buttonText,
+  contentClassName,
+  buttonClassName,
+}) {
   return (
-    <Popover content={content} title={title}>
-      <Button type='primary'>{buttonText}</Button>
+    <Popover
+      content={content}
+      title={title}
+      overlayClassName={contentClassName}
+    >
+      <Button className={buttonClassName} type='primary'>
+        {buttonText}
+      </Button>
     </Popover>
   );
 }

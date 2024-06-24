f7c9227440578096dea11451294010eb56c0bd44
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-06-23 21:19:01 -0400

    Slightly refactored LiveClock component and stylings for responsiveness

--- client/src/components/LiveClock/LiveClock.js
+++ client/src/components/LiveClock/LiveClock.js
@@ -1,26 +1,30 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+import './LiveClock.scss';
 import moment from 'moment';
 
 export default function LiveClock() {
-
   const [todaysDate, setTodaysDate] = useState(
     moment().format('dddd MMMM Do YYYY, h:mm:ss a')
   );
-
+  const [currentTime, setCurrentTime] = useState(moment().format('h:mm:ss a'));
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setTodaysDate(moment().format('dddd MMMM Do YYYY, hh:mm:ss a'));
+      setTodaysDate(moment().format('dddd MMMM Do YYYY'));
+      setCurrentTime(moment().format('hh:mm:ss a'));
     }, 1000);
 
     return () => {
       clearInterval(interval);
     };
-  }, [])
+  }, []);
 
   return (
     <>
-      {todaysDate}
+      <div className='liveClock-container'>
+        <div className='liveClock-date'>{todaysDate}</div>
+        <span className='liveClock-time'>{currentTime}</span>
+      </div>
     </>
-  )
+  );
 }

5a493331322f96ea46c0a9e07d712b3f1ac2266f
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-06-26 13:44:00 -0400

    feat: Add use IsMobile hook for detecting mobile devices for parallax components

---
+++ client/src/hooks/useIsMobile.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+
+function useIsMobile() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkIfMobile = () => {
+      setIsMobile(window.innerWidth <= 767); // max screen size
+    };
+
+    checkIfMobile();
+    window.addEventListener('resize', checkIfMobile);
+
+    return () => {
+      window.removeEventListener('resize', checkIfMobile);
+    };
+  }, []);
+
+  return isMobile;
+}
+
+export default useIsMobile;

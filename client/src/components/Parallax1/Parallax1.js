459899c9819a480869c9db4e09c57846fffc6b75
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-06-28 22:58:22 -0400

    refactored parallax1 component to utilize parallax styling on mobile devices

--- client/src/components/Parallax1/Parallax1.js
+++ client/src/components/Parallax1/Parallax1.js
@@ -1,13 +1,43 @@
 import './Parallax1.scss';
 import ParallaxImage1 from '../../assets/images/SafeHavenTO_graphic-art-skyline-1.png';
+import { useEffect, useRef } from 'react';
+import useIsMobile from '../../hooks/useIsMobile';
 
 export default function Parallax1() {
+  const isMobile = useIsMobile();
+  const parallaxRef = useRef(null);
+  const initialOffsetY = 40;
+  useEffect(() => {
+    const handleScroll = () => {
+      if (parallaxRef.current) {
+        const offset = window.scrollY;
+        parallaxRef.current.style.backgroundPositionY = `${
+          initialOffsetY + offset * -0.2
+        }px`;
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [initialOffsetY]);
+
   return (
     <>
-      <div
-        className='parallax1'
-        style={{ backgroundImage: `url(${ParallaxImage1})` }}
-      />
+      {isMobile ? (
+        <div
+          className='parallax1-mobile'
+          style={{
+            backgroundImage: `url(${ParallaxImage1})`,
+            backgroundPositionY: `${initialOffsetY}px`,
+          }}
+          ref={parallaxRef}
+        />
+      ) : (
+        <div
+          className='parallax1'
+          style={{ backgroundImage: `url(${ParallaxImage1})` }}
+        />
+      )}
     </>
   );
 }

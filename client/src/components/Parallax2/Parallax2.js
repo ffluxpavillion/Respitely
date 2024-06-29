7a9e6204baec7f8843ed5a32f1b7f1cf43240111
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-06-28 23:25:35 -0400

    refactor: Added new mobile parallax to Parallax2 component

--- client/src/components/Parallax2/Parallax2.js
+++ client/src/components/Parallax2/Parallax2.js
@@ -1,13 +1,43 @@
 import './Parallax2.scss';
 import ParallaxImage2 from '../../assets/images/SafeHavenTO_graphic-art-skyline-3.png';
+import { useEffect, useRef } from 'react';
+import useIsMobile from '../../hooks/useIsMobile';
 
 export default function Parallax2() {
+  const isMobile = useIsMobile();
+  const parallaxRef = useRef(null);
+  const initialOffsetY = 600;
+  useEffect(() => {
+    const handleScroll = () => {
+      if (parallaxRef.current) {
+        const offset = window.scrollY;
+        parallaxRef.current.style.backgroundPositionY = `${
+          initialOffsetY + offset * -0.2
+        }px`;
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [initialOffsetY]);
+
   return (
     <>
-      <div
-        className='parallax2'
-        style={{ backgroundImage: `url(${ParallaxImage2})` }}
-      ></div>
+      {isMobile ? (
+        <div
+          className='parallax2-mobile'
+          style={{
+            backgroundImage: `url(${ParallaxImage2})`,
+            backgroundPositionY: `${initialOffsetY}px`,
+          }}
+          ref={parallaxRef}
+        />
+      ) : (
+        <div
+          className='parallax2'
+          style={{ backgroundImage: `url(${ParallaxImage2})` }}
+        />
+      )}
     </>
   );
 }

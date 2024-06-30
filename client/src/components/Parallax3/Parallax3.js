e837a01aa64b2582f4e3a4bc61b3e254b2af2ef3
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-06-30 18:40:08 -0400

    Update Parallax3 component to implement parallax functionality on mobile devices as well

--- client/src/components/Parallax3/Parallax3.js
+++ client/src/components/Parallax3/Parallax3.js
@@ -1,13 +1,43 @@
 import './Parallax3.scss';
 import ParallaxImage3 from '../../assets/images/SafeHavenTO_graphic-art-skyline-2.png';
+import { useEffect, useRef } from 'react';
+import useIsMobile from '../../hooks/useIsMobile';
 
 export default function Parallax3() {
+  const isMobile = useIsMobile();
+  const parallaxRef = useRef(null);
+  const initialOffsetY = 850;
+  useEffect(() => {
+    const handleScroll = () => {
+      if (parallaxRef.current) {
+        const offset = window.scrollY;
+        parallaxRef.current.style.backgroundPositionY = `${
+          initialOffsetY + offset * -0.2
+        }px`;
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [initialOffsetY]);
+
   return (
     <>
-      <div
-        className='parallax3'
-        style={{ backgroundImage: `url(${ParallaxImage3})` }}
-      ></div>
+      {isMobile ? (
+        <div
+          className='parallax3-mobile'
+          style={{
+            backgroundImage: `url(${ParallaxImage3})`,
+            backgroundPositionY: `${initialOffsetY}px`,
+          }}
+          ref={parallaxRef}
+        />
+      ) : (
+        <div
+          className='parallax3'
+          style={{ backgroundImage: `url(${ParallaxImage3})` }}
+        />
+      )}
     </>
   );
 }

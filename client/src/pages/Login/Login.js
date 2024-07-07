9f7ac0ba4ce18d5edd11aceaf164a1fcc94288e9
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-07-06 22:41:04 -0400

    feat: Improve login page with form and styling updates

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,23 +1,41 @@
-import React from 'react'
-import './Login.scss'
+import { useState } from 'react';
+import './Login.scss';
 
 export default function Login() {
+  const [data, setData] = useState({
+    email: '',
+    password: '',
+  });
 
   const loginUser = (e) => {
     e.preventDefault();
-  }
+  };
   return (
     <>
       <section className='login-section'>
-        <h1 className='login-header'>LOGIN PAGE</h1>
-        <form onSubmit={loginUser}>
-          <label>Email</label>
-          <input type='email' placeholder='Enter your email' />
-          <label>Password</label>
-          <input type='password' placeholder='Enter your password' />
-          <button type='submit'>Login</button>
-        </form>
+        <div className='login-upper'>
+          <h1 className='login-header'>LOGIN PAGE</h1>
+        </div>
+        <div className='login-lower'>
+          <form className='login-form' onSubmit={loginUser}>
+            <label>Email</label>
+            <input
+              type='email'
+              placeholder='Enter your email'
+              value={data.email}
+              onChange={(e) => setData({ ...data, email: e.target.value })}
+            />
+            <label>Password</label>
+            <input
+              type='password'
+              placeholder='Enter your password'
+              value={data.password}
+              onChange={(e) => setData({ ...data, password: e.target.value })}
+            />
+            <button type='submit'>Login</button>
+          </form>
+        </div>
       </section>
     </>
-  )
+  );
 }

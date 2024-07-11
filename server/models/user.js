b5dc4281e5a58cc48e9d6e17dda95b22e63f9323
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-07-10 21:51:11 -0400

    feat: added server user model

---
+++ server/models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      unique: true,
+      match: [/.+\@.+\..+/, 'Please fill a valid email address'],
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const UserModel = mongoose.model('User', userSchema);
+
+module.exports = UserModel;

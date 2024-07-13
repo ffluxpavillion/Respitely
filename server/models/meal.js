12a788520658a696a710e9f21e747388e8df3a6c
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-07-12 21:53:09 -0400

    feat: Add Meal schema to server models

---
+++ server/models/meal.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+// Helper function to validate time format
+const validateTimeFormat = (time) => {
+  const regex = /^([01]\d|2[0-3]):([0-5]\d)$/; // HH:mm format
+  return regex.test(time);
+};
+
+const mealSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    enum: ['Breakfast', 'Lunch', 'Dinner', 'Snack'],
+    required: true
+  },
+  start_time: {
+    type: String,
+    required: true,
+    validate: {
+      validator: validateTimeFormat,
+      message: props => `${props.value} is not a valid time format! Use HH:mm.`
+    }
+  },
+  end_time: {
+    type: String,
+    required: true,
+    validate: {
+      validator: validateTimeFormat,
+      message: props => `${props.value} is not a valid time format! Use HH:mm.`
+    }
+  }
+});
+
+const Meal = mongoose.model('Meal', mealSchema);
+
+module.exports = Meal;

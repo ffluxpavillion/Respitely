dbe42fa6b288b0ca16eb7cafa1b49f555abf8b5f
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-07-14 23:24:03 -0400

    refactor: Update EmergencyBanner component with improved accessibility and formatting

--- client/src/components/EmergencyBanner/EmergencyBanner.js
+++ client/src/components/EmergencyBanner/EmergencyBanner.js
@@ -8,36 +8,42 @@ export default function EmergencyBanner() {
       role='region'
       aria-label='Emergency Banner Contact Information'
     >
+      <p className='banner-line-0'>*</p>
       <div className='banner-content'>
-        <i>
-          <u>
-            Shelter updates are for informational purposes only and does not
-            guarantee availability.
-          </u>
-        </i>{' '}
-        <br />
-        If you are in need of emergency shelter space, please call the City of
-        Toronto’s Central Intake line at
-        <a href='tel:4163384766' className='phone-link' aria-label='4163384766'>
-          {' '}
-          416-338-4766{' '}
-        </a>
-        or
-        <a
-          href='tel:18773383398'
-          className='phone-link'
-          aria-label='18773383398'
-        >
-          {' '}
-          1-877-338-3398.{' '}
-        </a>
-        For Street Outreach Services, please call
-        <a href='tel:311' className='phone-link' aria-label='311'>
-          {' '}
-          311
-        </a>
-        .
+        <p className='banner-line-1'>
+          Shelter updates are for informational purposes only and does not
+          guarantee availability.
+        </p>
+
+        <p className='banner-line-2'>
+          If you are in need of emergency shelter space, please call the City of
+          Toronto’s Central Intake line at <br />
+          <a
+            href='tel:4163384766'
+            className='phone-link'
+            aria-label='4163384766'
+          >
+            {' '}
+            416-338-4766{' '}
+          </a>
+          or
+          <a
+            href='tel:18773383398'
+            className='phone-link'
+            aria-label='18773383398'
+          >
+            {' '}
+            1-877-338-3398.{' '}
+          </a>
+          For Street Outreach Services, please call
+          <a href='tel:311' className='phone-link' aria-label='311'>
+            {' '}
+            311
+          </a>
+          .
+        </p>
       </div>
+      <p className='banner-line-0'>*</p>
     </div>
   );
 }

e1686b9c0ca080a10ea0f21429a621306dc174aa
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-07-22 22:30:03 -0400

    Rename to UserLoginMenu --component for user authentication and navigation

---
+++ client/src/components/UserLoginMenu/UserLoginMenu.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { DownOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Dropdown, message, Space, Tooltip, Spin } from 'antd';
+import { useAuth0 } from '@auth0/auth0-react';
+import './UserLoginMenu.scss';
+import LoginButton from '../LoginButton/LoginButton';
+
+export default function UserLoginMenu() {
+  const { user, logout, isAuthenticated, loginWithRedirect, isLoading } =
+    useAuth0();
+  const navigate = useNavigate();
+
+  const handleProfileClick = () => {
+    navigate('/profile');
+  };
+
+  const handleLogout = () => {
+    logout({
+      returnTo: window.location.origin,
+    });
+  };
+
+  const items = isAuthenticated
+    ? [
+        {
+          label: 'Dashboard',
+          key: '1',
+          icon: <UserOutlined />,
+          onClick: handleProfileClick,
+        },
+        {
+          label: 'My Profile',
+          key: '2',
+          icon: <UserOutlined />,
+          onClick: handleProfileClick,
+        },
+        {
+          label: 'Logout',
+          key: '3',
+          icon: <UserOutlined />,
+          danger: true,
+          onClick: handleLogout,
+        },
+      ]
+    : [
+        {
+          label: 'Signup / Login',
+          key: '1',
+          icon: <UserOutlined />,
+          onClick: loginWithRedirect,
+        },
+      ];
+
+  const menuProps = {
+    items,
+  };
+
+  if (isLoading) {
+    return (
+      <Space wrap>
+        <Button icon={<UserOutlined />}>
+          <Space>
+            <DownOutlined />
+          </Space>
+        </Button>
+      </Space>
+    );
+  }
+
+  return (
+    <Space wrap>
+      {isAuthenticated ? (
+        <Dropdown menu={menuProps}>
+          <Button icon={<UserOutlined />}>
+            <Space>
+              {isAuthenticated
+                ? `Welcome Back ${user.name}!`
+                : 'Drop-In Providers Signup / Login'}
+              <DownOutlined />
+            </Space>
+          </Button>
+        </Dropdown>
+      ) : (
+        <Button icon={<UserOutlined />} onClick={loginWithRedirect}>
+          <Space>
+            {isAuthenticated
+              ? `Welcome Back ${user.name}!`
+              : 'Drop-In Providers Signup / Login'}
+          </Space>
+        </Button>
+      )}
+    </Space>
+  );
+}

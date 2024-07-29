fb14c586fcebc08cf469d0cf1891b13dd8111b6a
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-07-28 22:14:58 -0400

    refactor: Move CORS middleware to separate file, added Mongo DB Connection, cookie parser, routes, etc. and general clean up

--- server/server.js
+++ server/server.js
@@ -1,74 +1,64 @@
 const express = require('express');
-const app = express();
-const cors = require('cors');
-const path = require('path');
 const dotenv = require('dotenv');
-
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const app = express(); // create an express app
+const connectToDatabase = require('./db/connection');
+const corsMiddleware = require('./middlewares/cors');
 
 // setting up env
-require('dotenv').config();   // Load the root-level .env
+require('dotenv').config(); // Load the root-level .env
 dotenv.config({ path: path.resolve(__dirname, './server/.env') });
-let { PORT } = process.env;
 
+// setting up port
+let { PORT } = process.env;
 PORT = process.env.PORT || 8081;
 
-// Enable CORS based on the environment
-if (process.env.NODE_ENV === 'development') { // Allow requests from any origin in development
-  app.use(cors());
-} else { // In production, restrict requests to the specified origin
-  const corsOptions = {
-      origin: process.env.CORS_ORIGIN,
-      optionsSuccessStatus: 200,
-  };
-  app.use(cors(corsOptions));
-}
+// CORS Middleware
+app.use(corsMiddleware); // Enable CORS
 
-
-// Serve static files only in production
-if (process.env.NODE_ENV === 'production') { // Serve static files from the React app
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-
-app.use(express.json()); // allows server to handle JSON data sent in req body
-
-// Middleware to set CORS headers for all responses
+// CORS Middleware -- set headers
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Allow any domain
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Origin', process.env.CORS_ORIGIN); // Allow any domain
+  // res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  // res.setHeader('Access-Control-Allow-Methods', 'GET');
   next();
 });
 
-const shelterRoutes = require('./routes/shelters'); // Routes
-app.use('/shelters', shelterRoutes)
+// Middleware
+app.use(express.json()); // automatically converts any request body we make to API Server, to JSON
+app.use(cookieParser()); // allows server to handle cookies
+app.use(express.urlencoded({ extended: false })); // allows server to handle form data
+
+// Connect to MongoDB
+connectToDatabase();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build'))); // Serve static files from the React app
+}
+
+// Routes
+// const authRoutes = require('./routes/auth-routes');
+const shelterRoutes = require('./routes/shelters');
+const mealRoutes = require('./routes/meals');
+
+// app.use(authRoutes);
+app.use('/shelters', shelterRoutes);
+app.use('/meals', mealRoutes);
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the SafeHavenTO Server!');
+  res.send('Welcome to the Respitely Server!');
 });
 
 app.get('/api/maps-key', (req, res) => {
-  res.json({key: process.env.REACT_APP_MAPBOX});
-});
-
-// Catch-all route for testing
-app.get('*', (req, res) => {
-  res.status(404).send('404 Not Found');
+  res.json({ key: process.env.REACT_APP_MAPBOX });
 });
 
 app.use((req, res) => {
-  res.send('This is not a valid route. Try <b>/shelters</b> instead.');
+  res.send('This is not a valid route. Try <b>/shelters</b> or  <b>/meals/toronto/:day</b> or <b>/meals/vancouver/:day</b>  instead.');
 });
 
-// // redirect from safehavento.org to respitely.org
-// app.use((req, res, next) => {
-//   if (req.hostname === 'safehavento.org' || req.hostname === 'www.safehavento.org') {
-//     res.redirect(301, 'https://respitely.org' + req.originalUrl);
-//   } else {
-//     next();
-//   }
-// });
-
+// Start the server
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
   console.log('Press CTRL + C to stop server');

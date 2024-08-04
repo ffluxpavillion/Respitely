e287b7da48759156d99fa0b13d1dedec28e28d4a
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-08-03 20:38:04 -0400

    feat: Updated meals routes -- added POST DELETE and PATCH with placeholder responses, Postman tested -working

--- server/routes/meals.js
+++ server/routes/meals.js
@@ -2,7 +2,22 @@ const router = require('express').Router();
 const { getMealsByDay, getAllMealsByCity } = require('../controllers/meal-controller');
 
 // Routes
-router.get('/:city/:day', getMealsByDay);
-router.get('/:city', getAllMealsByCity);
+router.get('/:city/meals', getAllMealsByCity); // GET all meals for a city
+router.get('/:city/meals/:day', getMealsByDay); // GET all meals for a city by day
+
+// POST a single meal
+router.post('/:city/meals', (req, res) => {
+  res.json({mssg: 'POST a new meal'});
+})
+
+// DELETE a single meal
+router.delete('/:city/meals/:mealId', (req, res) => {
+  res.json({ mssg: 'DELETE a meal' });
+})
+
+// UPDATE a single meal
+router.patch('/:city/meals/:mealId', (req, res) => {
+  res.json({ mssg: 'UPDATE a meal' });
+})
 
 module.exports = router;

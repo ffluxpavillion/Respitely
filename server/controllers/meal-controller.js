24c04de97668c392a379f3166c921931832b7b93
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-08-04 19:25:56 -0400

    feat: Added getMeal function to fetch single provider info by id

--- server/controllers/meal-controller.js
+++ server/controllers/meal-controller.js
@@ -1,6 +1,6 @@
 const TorontoMeal = require('../models/toronto-meal');
 const VancouverMeal = require('../models/vancouverMeal');
-const geocodeAddress = require('../helpers/geocode')
+const geocodeAddress = require('../helpers/geocode');
 
 const validCities = ['toronto', 'vancouver'];
 const validDays = [
@@ -13,9 +13,10 @@ const validDays = [
   'sunday',
 ];
 
-// ------------------------ (GET) FETCH MEALS ------------------------
+// ------------------------ (GET) ALL MEALS -- BY CITY, BY CITY AND DAY OF THE WEEK ------------------------
 
-const getMeals = async (req, res) => { // GET all meals for a city
+const getMeals = async (req, res) => {
+  // GET all meals for a city
   const city = req.params.city.toLowerCase();
   const day = req.query.day ? req.query.day.toLowerCase() : null;
   const MealModel = city === 'toronto' ? TorontoMeal : VancouverMeal;
@@ -29,19 +30,28 @@ const getMeals = async (req, res) => { // GET all meals for a city
     return res.status(400).json({ error: 'Invalid day parameter' });
   }
 
-  try { // Fetch provider information + drop-in meal schedule for specific day-- /api/v1/:city/meals?day=:day
-    let query = { };
+  try {
+    // Fetch provider information + drop-in meal schedule for specific day -- /api/v1/:city/meals?day=:day
+    let query = {};
     if (day) {
       query[`schedule.${day}`] = { $exists: true };
 
       const meals = await MealModel.find(query);
-      console.log(`${city} Meals fetched${day ? ` for ${day}` : ''}:`, meals.length);
+      console.log(
+        `${city} Meals fetched${day ? ` for ${day}` : ''}:`,
+        meals.length
+      );
 
       if (!meals.length) {
-        return res.status(404).json({ error: `No meal data found for ${city}${day ? ` on ${day}` : ''}` });
+        return res
+          .status(404)
+          .json({
+            error: `No meal data found for ${city}${day ? ` on ${day}` : ''}`,
+          });
       }
 
-      const response = meals.map(meal => { // Prepare response to include only the schedule for the requested day
+      const response = meals.map((meal) => {
+        // Prepare response to include only the schedule for the requested day
         const mealObj = meal.toObject();
         return {
           id: mealObj._id,
@@ -56,49 +66,104 @@ const getMeals = async (req, res) => { // GET all meals for a city
           wheelchair_accessible: mealObj.wheelchair_accessible,
           schedule: day ? { [day]: mealObj.schedule[day] } : mealObj.schedule,
           claimed_by: mealObj.claimed_by,
-          last_updated: mealObj.last_updated
+          last_updated: mealObj.last_updated,
         };
       });
 
       res.json(response);
+    } else { // Else, fetch provider info + ENTIRE drop-in meal schedule -- /api/v1/:city/meals
 
-  } else { // Fetch location information + full drop-in meal schedule -- /api/v1/:city/meals
-    const meals = await MealModel.find({});
-    console.log(`${city} Meals fetched:`, meals.length);
+      const meals = await MealModel.find({}).sort({ createdAt: -1 });
+      console.log(`${city} Meals fetched:`, meals.length);
 
-    if (!meals.length) {
-      return res.status(404).json({ error: `No meal data found for ${city}` });
+      if (!meals.length) {
+        return res
+          .status(404)
+          .json({ error: `No meal data found for ${city}` });
+      }
+
+      res.json(meals); // Return all meals
+    }
+  } catch (err) {
+    console.error(
+      `Error fetching meals for ${city}${day ? ` on ${day}` : ''}:`,
+      err
+    );
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// ------------------------ (GET) SINGLE MEAL -- BY ID ------------------------
+const getMeal = async (req, res) => {
+  const city = req.params.city.toLowerCase();
+  const id = req.params.id;
+  const MealModel = city === 'toronto' ? TorontoMeal : VancouverMeal;
+
+  if (!validCities.includes(city)) {
+    // Validate city parameter
+    return res.status(400).json({ error: 'Invalid city parameter' });
+  }
+
+  try {
+    // Fetch meal by ID
+    const meal = await MealModel.findById(id);
+
+    if (!meal) {
+      // Return error if meal not found
+      return res
+        .status(404)
+        .json({ error: `No meal data found with ID ${id} in ${city}` });
     }
 
-    res.json(meals); // Return all meals
-  }
-  } catch (err) {
-    console.error(`Error fetching meals for ${city}${day ? ` on ${day}` : ''}:`, err);
+    res.status(200).json(meal);
+  } catch (error) {
+    console.error(`Error fetching meal with ID ${id} in ${city}:`, err);
     res.status(500).json({ error: err.message });
   }
 };
 
 // ------------------------ (POST) CREATE MEALS ------------------------
 
-  const createMeal = async ( req, res ) => {
-  const {name, address, contact, population, notes, service_dog_allowed, wheelchair_accessible, schedule, claimed_by} = req.body;
+const createMeal = async (req, res) => {
+  const { // destructure req.body
+    name,
+    address,
+    contact,
+    population,
+    notes,
+    service_dog_allowed,
+    wheelchair_accessible,
+    schedule,
+    claimed_by,
+  } = req.body;
 
-  // add doc to db
+
   try {
     // geocode address to get lat and long
     const { latitude, longitude } = await geocodeAddress(address);
 
     // add doc to db
-    const meal = await TorontoMeal.create({name, address, latitude, longitude, contact, population, notes, service_dog_allowed, wheelchair_accessible, schedule, claimed_by})
-    res.status(200).json(meal)
+    const meal = await TorontoMeal.create({ // create new meal
+      name,
+      address,
+      latitude,
+      longitude,
+      contact,
+      population,
+      notes,
+      service_dog_allowed,
+      wheelchair_accessible,
+      schedule,
+      claimed_by,
+    });
+    res.status(200).json(meal);
   } catch (error) {
-    res.status(400).json({error: error.message})
-
+    res.status(400).json({ error: error.message });
   }
-}
-
+};
 
 module.exports = {
   getMeals,
-  createMeal
+  getMeal,
+  createMeal,
 };

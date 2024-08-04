4d03284d75fe0f480ec4c4759e6c559c1f611622
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-08-04 18:29:08 -0400

    refactor: Update meals routes to use createMeal function for POST requests

--- server/routes/meals.js
+++ server/routes/meals.js
@@ -1,16 +1,13 @@
 const router = require('express').Router();
-const { getMeals } = require('../controllers/meal-controller');
+const { getMeals, createMeal } = require('../controllers/meal-controller');
 
 // Routes
 
 // GET all meals for a city, or query by day of the week
 router.get('/:city/meals', getMeals);
 
-
 // POST a single meal
-router.post('/:city/meals', (req, res) => {
-  res.json({mssg: 'POST a new meal'});
-})
+router.post('/toronto/meals', createMeal);
 
 // DELETE a single meal
 router.delete('/:city/meals/:mealId', (req, res) => {

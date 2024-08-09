8ed43ce39613c4aaa3c750f5688a8c708395492f
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-08-08 23:41:47 -0400

    feat: Refactor MealsTimeline component to fetch and display meals from server --Moved data fetching to parent component, for prop use in children

--- client/src/components/MealsRemake/MealsTimeLine/MealsTimeLine.js
+++ client/src/components/MealsRemake/MealsTimeLine/MealsTimeLine.js
@@ -20,12 +20,8 @@ import axios from 'axios';
 
 const { Panel } = Collapse;
 
-const MealsTimeline = () => {
+const MealsTimeline = ({ timelineItems, getDirectionsUrl }) => {
   const { locationInfo } = useGeolocation();
-  const [timelineItems, setTimelineItems] = useState([]);
-  const [currentEvents, setCurrentEvents] = useState([]);
-  const [previousEvent, setPreviousEvent] = useState(null);
-  const [nextEvents, setNextEvents] = useState([]);
   const [isTimelineVisible, setIsTimelineVisible] = useState(true); // State to control timeline visibility
   const timelineContainerRef = useRef(null);
   const currentEventRef = useRef(null);
@@ -33,89 +29,6 @@ const MealsTimeline = () => {
   const [data, setData] = useState([]);
 
 
-  useEffect(() => { // Fetch all drop-ins, with only today's schedules from the server
-    const today = moment().format('dddd').toLowerCase();
-    const fetchDropins = async () => {
-      try {
-        const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/v1/toronto/meals?day=${today}`);
-        const data = response.data;
-        setData(data);
-        console.log('DROP-INS FROM SERVER=====', data);
-
-        processMeals(data, today);
-
-      } catch (error) {
-        console.error('Error fetching drop-ins:', error);
-      }
-    };
-
-    fetchDropins();
-  }, []);
-
-
-
-  const processMeals = (data, today) => { // Filter to include schedules for today only and sort by start time
-    let mealsForToday = [];
-    const mealTypes = ['breakfast', 'lunch', 'dinner', 'snack'];
-    const filteredData = data.filter((meal) => meal.schedule[today] && meal.schedule[today].meals);
-    console.log('FILTERED DATA=====', filteredData);
-
-    filteredData.forEach((provider) => { // For each provider
-      const schedule = provider.schedule[today].meals;
-
-      mealTypes.forEach((mealType) => { // Then, for each meal type, create an entry if it exists,
-        if (schedule[mealType]) {
-          const mealEntries = schedule[mealType];
-
-            const entry = mealEntries;
-            const now = moment();
-            const startTime = moment(mealEntries.start, "h:mm a").toDate(); // convert 24-hour clock to 12 hour AM/PM
-            const endTime = moment(mealEntries.end, "h:mm a").toDate(); // convert 24-hour clock to 12 hour AM/PM
-            const isCurrent = now.isBetween(startTime, endTime);
-            const isEnded = now.isAfter(endTime);
-            const isComingUp = now.isBefore(startTime) && moment(startTime).diff(now, 'hours') <= 2;
-
-            mealsForToday.push({ // and add the entry to the mealsForToday array
-              typeOfMeal: mealType.charAt(0).toUpperCase() + mealType.slice(1),
-              timeOfMeal: `${moment(startTime).format('h:mma')} - ${moment(endTime).format('h:mma')}`,
-              providerOfMeal: provider.name,
-              addressOfMeal: `${provider.address.street}, ${provider.address.city}, ${provider.address.province}, ${provider.address.postal_code}`,
-              startTime,
-              endTime,
-              isCurrent: isCurrent,
-              isComingUp: isComingUp,
-              isEnded: isEnded,
-              wheelchair_accessible: provider.wheelchair_accessible,
-              service_dog_allowed: provider.service_dog_allowed,
-              latitude: provider.latitude,
-              longitude: provider.longitude,
-              ...provider
-            });
-        }
-      });
-    });
-
-    const sortedMeals = mealsForToday.sort((a, b) => a.startTime - b.startTime);
-    setTimelineItems(sortedMeals);
-
-    const currentEvents = sortedMeals.filter((meal) => meal.isCurrent);
-    const previousEventIndex = sortedMeals.findIndex((meal) => meal.isEnded) - 1;
-    const nextEvents = sortedMeals.filter((meal) => meal.isComingUp);
-
-    // console.log('SORTED MEALS=====', sortedMeals);
-
-    setCurrentEvents(currentEvents);
-    setPreviousEvent(
-      previousEventIndex !== null && previousEventIndex >= 0 ? sortedMeals[previousEventIndex] : null
-    );
-    setNextEvents(nextEvents);
-  };
-
-  const getDirectionsUrl = (providerOfMeal, addressOfMeal) => {
-    return `https://www.google.com/maps/dir/?api=1&destination=${encodeURIComponent(
-      `${providerOfMeal} ${addressOfMeal}`
-    )}&travelmode=walking`;
-  };
 
   return (
     <>

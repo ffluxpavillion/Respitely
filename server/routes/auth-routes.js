1645c4e872bc5c7c1e175ef3627f6649858a1420
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-08-13 23:07:13 -0400

    refactor: Remove unused middleware in auth-routes.js

--- server/routes/auth-routes.js
+++ server/routes/auth-routes.js
@@ -3,13 +3,6 @@ const router = express.Router();
 const cors = require('cors');
 const { test, registerUser, loginUser, getProfile } = require('../controllers/auth-controller');
 
-// middleware
-router.use(
-  cors({
-    credentials: true,
-    origin: 'http://localhost:3000'
-  })
-)
 
 // router.get('/', test)
 router.post('/register', registerUser)

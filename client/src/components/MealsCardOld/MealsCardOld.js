072b102092b985556c87eaf7ef01e7743e555e5c
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2024-08-15 10:30:00 -0700

    refactor: Rename to MealsCardOld for total refactor of component to use database data

---
+++ client/src/components/MealsCardOld/MealsCardOld.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './MealsCardOld.scss';
+import DropInMealsToday from '../DropInMealsToday/DropInMealsToday';
+import MealsTimelineOld from '../MealsTimelineOld/MealsTimelineOld';
+import MealsBanner from '../MealsBanner/MealsBanner';
+import { HashLink as Link } from 'react-router-hash-link';
+
+import ComingSoon from '../ComingSoon/ComingSoon';
+
+export default function MealsCardOld() {
+  const scrollToTop = (el) => {
+    // need to fix this
+    el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  };
+
+  return (
+    <>
+      <MealsBanner />
+      <section className='meals-section' id='dropInMeals'>
+        <div className='meals-section__upper'>
+          <h3 className='meals-section__header' id='meals'>
+            Toronto Drop-In Meals Daily Tracker
+          </h3>
+
+          {/* <DropInMealsToday /> */}
+          <MealsTimelineOld />
+        </div>
+        <div className='meals-section__lower'>
+          <Link to='/drop-in-map' scroll={scrollToTop} aria-label='Link to Drop-In Map'>
+            <h3 className='meals-section__map-link'>
+              EXPLORE DROP-IN MAP ⟩⟩
+            </h3>
+          </Link>
+        </div>
+        {/* <ComingSoon
+            title="New Drop-In Meals UI Coming Soon!"
+            message="A more intuitive solution is on the way -- this upcoming tool will provide essential info on drop-in meals, helping you locate services quickly and efficiently.  Sincerely grateful for the patience and support, stay safe."
+            height='100%'
+            width='80%'
+        /> */}
+      </section>
+    </>
+  );
+}

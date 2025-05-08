a2f6b94c61d4e275e71d7cb55bf43390d8ecfefd
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2025-05-08 00:08:38 -0400

    code refactor.  trying alternative simpler approach

--- server/db/connection.js
+++ server/db/connection.js
@@ -12,3 +12,31 @@ const connectToDatabase = async () => {  // Connect to MongoDB
 };
 
 module.exports = connectToDatabase;
+
+
+// const mongoose = require('mongoose');
+
+// const connections = {};
+
+// const connectToDatabase = async () => {
+//   const dbUris = {
+//     toronto: process.env.MONGODB_URI_TORONTO,
+//     vancouver: process.env.MONGODB_URI_VANCOUVER,
+//     // mississauga: process.env.MONGODB_URI_MISSISSAUGA, // hypothetical for the future
+//     // montreal: process.env.MONGODB_URI_MONTREAL, // hypothetical for the future
+//     // ottawa: process.env.MONGODB_URI_OTTAWA // hypothetical for the future
+//   };
+
+//   for (const city in dbUris) {
+//     if (dbUris[city]) {
+//       try {
+//         connections[city] = await mongoose.createConnection(dbUris[city], { useNewUrlParser: true, useUnifiedTopology: true });
+//         console.log(`Connected to MongoDB - database for ${city}`);
+//       } catch (error) {
+//         console.error(`Error connecting to MongoDB for ${city}:`, error);
+//       }
+//     }
+//   }
+// };
+
+// module.exports = { connectToDatabase, connections };

3b99945622709dbe5192f3adb1fa5fc0499d36d1
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2025-05-08 00:10:03 -0400

    added Provider to module.exports

--- server/models/index.js
+++ server/models/index.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
-const DropInCenter = require('./drop-in-center');
 const Meal = require('./meal');
-const DaySchedule = require('./day-schedule');
-const TemporarySchedule = require('./temporary-schedule');
 
 module.exports = {
-  DropInCenter,
+  Provider,
   Meal,
   DaySchedule,
   TemporarySchedule

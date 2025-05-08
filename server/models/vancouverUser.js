c68f5a92751e2ad09b221b97261c6c1b01d7a46a
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2025-05-08 00:12:10 -0400

    fix typo

--- server/models/vancouverUser.js
+++ server/models/vancouverUser.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-const VancouverUser = mongoose.createConnection(process.env.MONGO_DB_URI_VANCOUVER).model('User', userSchema);
+const VancouverUser = mongoose.createConnection(process.env.MONGO_DB_URI_VANCOUVER).model('users', userSchema);
 
 module.exports = VancouverUser;

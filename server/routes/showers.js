acc17750e65db43b9dc3cebc97fd31074b7f8f33
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2025-05-08 00:16:05 -0400

    added routes

--- server/routes/showers.js
+++ server/routes/showers.js
@@ -5,4 +5,11 @@ router.get('/:city/showers', (req, res) => {
   res.json({ mssg: `GET all ${city} showers` });
 });
 
+router.get('/:city/showers/:day', (req, res) => {
+  const city = req.params.city;
+  const day = req.params.day;
+  res.json({ mssg: `GET all ${city} showers ${day.charAt(0).toUpperCase() + day.substring(1)}` });
+});
+
+
 module.exports = router;

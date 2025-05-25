196e0e4d05c8ac347aa18d3ff7a39f51e4ce4127
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2025-05-25 19:11:37 -0400

    added form items to registration / request access page

--- client/src/pages/Register/Register.js
+++ client/src/pages/Register/Register.js
@@ -7,9 +7,15 @@ import { useNavigate } from 'react-router-dom';
 export default function Register() {
   const navigate = useNavigate();
   const [data, setData] = useState({
-    name: '',
+    firstName: '',
+    lastName: '',
+    orgName: '',
     email: '',
-    password: '',
+    phoneNumber: '',
+    website: '',
+    affiliatedService: '',
+    proof: '',
+    // password: '',
   });
 
   const registerUser = async (e) => {
@@ -43,41 +49,88 @@ export default function Register() {
           </h1>
           <br />
           <p className='register-text'>
-            Welcome to the Drop-In Service Provider Registration Page.
+            Welcome to the Provider Registration Page.
             <br />
             <br />
-            Please complete the form below to register your organization with
-            Respitely. Once registered, you will be able to add your
-            organization to the drop-in map, and update your information as
-            needed.
+            Please complete the form below to request access. Once verified and registered, you can add your
+            meal service to the platform and update details as needed.
           </p>
         </div>
+        <br />
 
         <form className='register-form' onSubmit={registerUser}>
+        <label>First Name:</label>
+          <input
+            className='register-input'
+            type='text'
+            placeholder='Enter first name'
+            value={data.name}
+            onChange={(e) => setData({ ...data, firstName: e.target.value })}
+          />
+          <label>Last Name:</label>
+          <input
+            className='register-input'
+            type='text'
+            placeholder='Enter last name'
+            value={data.name}
+            onChange={(e) => setData({ ...data, lastName: e.target.value })}
+          />
           <label>Organization Name:</label>
           <input
             className='register-input'
             type='text'
             placeholder='Enter organization name'
             value={data.name}
-            onChange={(e) => setData({ ...data, name: e.target.value })}
+            onChange={(e) => setData({ ...data, orgName: e.target.value })}
           />
           <label>Organization Email:</label>
           <input
             className='register-input'
             type='email'
-            placeholder='Enter email'
+            placeholder='Enter organization email'
             value={data.email}
             onChange={(e) => setData({ ...data, email: e.target.value })}
           />
-          <label>Password</label>
+          <label>Phone Number:</label>
+          <input
+            className='register-input'
+            type='text'
+            placeholder='Enter phone number'
+            value={data.name}
+            onChange={(e) => setData({ ...data, phoneNumber: e.target.value })}
+          />
+          <label>Website:</label>
+          <input
+            className='register-input'
+            type='text'
+            placeholder='Enter URL'
+            value={data.name}
+            onChange={(e) => setData({ ...data, website: e.target.value })}
+          />
+          <label>Which meal service are you affiliated with?</label>
+          <input
+            className='register-input'
+            type='text'
+            placeholder='DROP DOWN MENU HERE'
+            value={data.name}
+            onChange={(e) => setData({ ...data, affiliatedService: e.target.value })}
+          />
+          <label>Proof of Affiliation</label>
+          <input
+            className='register-input'
+            type='text'
+            placeholder='FILE UPLOAD OR EXPLANATION HERE'
+            value={data.name}
+            onChange={(e) => setData({ ...data, proof: e.target.value })}
+          />
+          {/* <label>Password</label>
           <input
             className='register-input'
             type='password'
             placeholder='Enter password'
             value={data.password}
             onChange={(e) => setData({ ...data, password: e.target.value })}
-          />
+          /> */}
           <button type='submit'>Submit</button>
         </form>
       </section>

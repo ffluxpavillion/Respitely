e3c080f9f0f16bb447703b4b23a2337d3715baae
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2025-06-08 22:24:29 -0400

    added provider request patch routes

--- server/routes/provider-request.js
+++ server/routes/provider-request.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const { createProviderRequest, getProviderRequests } = require('../controllers/provider-controller');
+const { createProviderRequest, getProviderRequests, updateProviderRequestStatus } = require('../controllers/provider-controller');
 
 router.post('/:city/provider-requests', createProviderRequest);
 router.get('/:city/provider-requests', getProviderRequests);
-
+router.patch('/:city/provider-requests/:id/status', updateProviderRequestStatus);
 
 module.exports = router;

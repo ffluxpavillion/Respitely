dde0428a5372f89fad7566cfcf27c9ad8f8c7b4a
Arjun Sahjpaul <51818329+ffluxpavillion@users.noreply.github.com>
2025-06-08 22:24:12 -0400

    added controller functionality to handle and update approve/deny requests into mongo db

--- server/controllers/provider-controller.js
+++ server/controllers/provider-controller.js
@@ -1,7 +1,7 @@
 const ProviderRequest = require('../models/provider-request');
 
 
-// ------------------------ (POST) PROVIDER REQUEST ------------------------ -- /api/v1/:city/provier-request
+// ------------------------ (POST) PROVIDER REQUEST ------------------------ -- /api/v1/:city/provier-requests
 
 const createProviderRequest = async (req, res) => {
   try {
@@ -42,7 +42,8 @@ const createProviderRequest = async (req, res) => {
 const getProviderRequests = async (req, res) => {
   try {
     // Filter by city (req.params.city)
-    const requests = await ProviderRequest.find().lean();
+    const requests = await ProviderRequest.find()
+    .lean();
     res.json(requests);
   } catch (error) {
     console.error('Error fetching provider requests:', error);
@@ -50,5 +51,32 @@ const getProviderRequests = async (req, res) => {
   }
 };
 
+// ------------------------ (PATCH) PROVIDER STATUS REQUESTS ------------------------ -- /api/v1/:city/provider-requests
 
-module.exports = {createProviderRequest, getProviderRequests}
+const updateProviderRequestStatus = async (req, res) => {
+  const { id } = req.params;
+  const { status, internalNotes } = req.body;
+
+  try {
+    const updated = await ProviderRequest.findByIdAndUpdate(
+      id,
+      {
+        status,
+        ...(internalNotes !== undefined && { internalNotes })
+      },
+      { new: true }
+    );
+
+    if (!updated) {
+      return res.status(404).json({ message: 'Request not found' });
+    }
+
+    res.json(updated);
+  } catch (err) {
+    console.error('Error updating access request status:', err);
+    res.status(500).json({ message: 'Failed to update status' });
+  }
+  console.log('Updating request', id, status, internalNotes);
+};
+
+module.exports = {createProviderRequest, getProviderRequests, updateProviderRequestStatus}
